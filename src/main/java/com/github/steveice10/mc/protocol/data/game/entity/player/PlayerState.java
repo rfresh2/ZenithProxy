c5cdbec20d665c4d481e3c22f836f372ffc2c2c0
DaPorkchop_ <joey.rabil@gmail.com>
2017-06-26 17:35:31 +0200

    fix opening horse inventory

---
+++ src/main/java/com/github/steveice10/mc/protocol/data/game/entity/player/PlayerState.java
@@ -0,0 +1,13 @@
+package com.github.steveice10.mc.protocol.data.game.entity.player;
+
+public enum PlayerState {
+    START_SNEAKING,
+    STOP_SNEAKING,
+    LEAVE_BED,
+    START_SPRINTING,
+    STOP_SPRINTING,
+    START_HORSE_JUMP,
+    STOP_HORSE_JUMP,
+    OPEN_HORSE_INVENTORY,
+    START_ELYTRA_FLYING;
+}
