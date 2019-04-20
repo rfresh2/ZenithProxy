a04634b1c5afdf189c5cb216eafd0720bf40ad93
DaMatrix <joey.rabil@gmail.com>
2019-04-20 17:30:13 +0200

    correctly handle relative position from server

--- src/main/java/net/daporkchop/toobeetooteebot/client/handler/incoming/PlayerPosRotHandler.java
+++ src/main/java/net/daporkchop/toobeetooteebot/client/handler/incoming/PlayerPosRotHandler.java
@@ -16,9 +16,11 @@
 
 package net.daporkchop.toobeetooteebot.client.handler.incoming;
 
+import com.github.steveice10.mc.protocol.data.game.entity.player.PositionElement;
 import com.github.steveice10.mc.protocol.packet.ingame.server.entity.player.ServerPlayerPositionRotationPacket;
 import lombok.NonNull;
 import net.daporkchop.toobeetooteebot.client.PorkClientSession;
+import net.daporkchop.toobeetooteebot.util.cache.data.PlayerCache;
 import net.daporkchop.toobeetooteebot.util.handler.HandlerRegistry;
 
 /**
@@ -27,12 +29,13 @@ import net.daporkchop.toobeetooteebot.util.handler.HandlerRegistry;
 public class PlayerPosRotHandler implements HandlerRegistry.IncomingHandler<ServerPlayerPositionRotationPacket, PorkClientSession> {
     @Override
     public boolean apply(@NonNull ServerPlayerPositionRotationPacket packet, @NonNull PorkClientSession session) {
-        CACHE.getPlayerCache()
-                .setX(packet.getX())
-                .setY(packet.getY())
-                .setZ(packet.getZ())
-                .setYaw(packet.getYaw())
-                .setPitch(packet.getPitch());
+        PlayerCache cache = CACHE.getPlayerCache();
+        cache
+                .setX((packet.getRelativeElements().contains(PositionElement.X) ? cache.getX() : 0.0d) + packet.getX())
+                .setY((packet.getRelativeElements().contains(PositionElement.Y) ? cache.getY() : 0.0d) + packet.getY())
+                .setZ((packet.getRelativeElements().contains(PositionElement.Z) ? cache.getZ() : 0.0d) + packet.getZ())
+                .setYaw((packet.getRelativeElements().contains(PositionElement.YAW) ? cache.getYaw() : 0.0f) + packet.getYaw())
+                .setPitch((packet.getRelativeElements().contains(PositionElement.PITCH) ? cache.getPitch() : 0.0f) + packet.getPitch());
         return true;
     }
 
