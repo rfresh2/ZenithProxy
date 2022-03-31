9dbdb7cbc5009c4b07843e471f7d6f5ea651706e
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2022-03-30 22:03:13 -0700

    ok try this

--- src/main/java/com/zenith/discord/command/ActiveHoursCommand.java
+++ src/main/java/com/zenith/discord/command/ActiveHoursCommand.java
@@ -51,9 +51,7 @@ public class ActiveHoursCommand extends Command {
                     .title("Active Hours Status")
                     .color(Color.CYAN)
                     .addField("Time Zone", activeHoursConfig.timeZoneId, false)
-//                    .addField("Active Hours", (activeHoursConfig.activeTimes.isEmpty() ? "None set!" : activeHoursConfig.activeTimes.stream()
-//                                    .collect(Collectors.joining(", "))),
-//                            false)
+                    .addField("Active Hours", (activeHoursConfig.activeTimes.isEmpty() ? "None set!" : String.join(", ", activeHoursConfig.activeTimes)), false)
                     .addField("Force Reconnect", (activeHoursConfig.forceReconnect ? "on" : "off"), false);
         } else if (commandArgs.get(1).equalsIgnoreCase("on")) {
             activeHoursConfig.enabled = true;
