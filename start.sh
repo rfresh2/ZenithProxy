17b110c1813e393ed7d481bed77ca8e79ae92890
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2022-04-10 13:16:13 -0700

    resolve jar build problems after git merge

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ do
   "$BUILD_SCRIPT"
 	# The ParNewGC only has marginally better YoungGen collection time at the expense of MT overhead.
 	# -XX:+UseParNewGC -XX:ConcGCThreads=4 -XX:ParallelGCThreads=4
-	java -server -Xmx180M -XX:NewSize=80M -D java.util.concurrent.ForkJoinPool.common.parallelism=16 -XX:+UseSerialGC -jar -Djava.util.concurrent.ForkJoinPool.common.parallelism=16 build/libs/Proxy.jar
+	java -server -Xmx180M -XX:NewSize=80M -XX:+UseSerialGC -jar -Djava.util.concurrent.ForkJoinPool.common.parallelism=16 build/libs/Proxy.jar
 	echo "Restarting. Press Ctrl+C to stop"
 	sleep 3
 done
