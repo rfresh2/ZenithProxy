60993e5d5373a3e26cd27d4cbfe3ee5211da5b5d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2022-10-30 19:36:41 -0700

    use plain console for run

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ BUILD_SCRIPT="$(pwd)/$(dirname $0)/build.sh"
 while true
 do
   "$BUILD_SCRIPT"
-	./gradlew run
-	echo "Restarting. Press Ctrl+C to stop"
-	sleep 3
+  ./gradlew run --console=plain
+  echo "Restarting. Press Ctrl+C to stop"
+  sleep 3
 done
