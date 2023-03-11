e7f57b9d85a43c3f7372cb6d135e191795810c3c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-03-10 20:12:52 -0800

    ensure we sync chunks if player joins immediately after q ends

--- src/main/java/com/zenith/server/handler/player/postoutgoing/JoinGamePostHandler.java
+++ src/main/java/com/zenith/server/handler/player/postoutgoing/JoinGamePostHandler.java
@@ -2,11 +2,16 @@ package com.zenith.server.handler.player.postoutgoing;
 
 import com.github.steveice10.mc.protocol.packet.ingame.server.ServerJoinGamePacket;
 import com.github.steveice10.mc.protocol.packet.ingame.server.ServerPluginMessagePacket;
+import com.zenith.Proxy;
 import com.zenith.cache.DataCache;
+import com.zenith.cache.data.chunk.ChunkCache;
 import com.zenith.server.ServerConnection;
 import com.zenith.util.RefStrings;
 import com.zenith.util.handler.HandlerRegistry;
 import lombok.NonNull;
+
+import java.time.Duration;
+import java.time.Instant;
 
 import static com.zenith.util.Constants.CACHE;
 
@@ -27,7 +32,13 @@ public class JoinGamePostHandler implements HandlerRegistry.PostOutgoingHandler<
                     session.send(connection.getEntityMetadataPacket());
                 });
 
-        session.setLoggedIn(true);
+        session.setLoggedIn(true); // allows server packets to start being send to player
+
+        // ensure we actually fully sync chunks if player is immediately connecting on proxy connect
+        // avoids race conditions especially when proxy exits prio q right as player joins
+        if (Proxy.getInstance().getConnectTime().isAfter(Instant.now().minus(Duration.ofSeconds(30)))) {
+            ChunkCache.sync();
+        }
     }
 
     @Override
