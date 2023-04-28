c39d083a73de75dc6ed44af9184b94d35effa5eb
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-28 15:40:51 -0700

    Warn about extrachat settings on login

--- src/main/java/com/zenith/server/handler/player/postoutgoing/JoinGamePostHandler.java
+++ src/main/java/com/zenith/server/handler/player/postoutgoing/JoinGamePostHandler.java
@@ -1,5 +1,6 @@
 package com.zenith.server.handler.player.postoutgoing;
 
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerChatPacket;
 import com.github.steveice10.mc.protocol.packet.ingame.server.ServerJoinGamePacket;
 import com.github.steveice10.mc.protocol.packet.ingame.server.ServerPluginMessagePacket;
 import com.zenith.cache.DataCache;
@@ -9,6 +10,7 @@ import com.zenith.util.handler.HandlerRegistry;
 import lombok.NonNull;
 
 import static com.zenith.util.Constants.CACHE;
+import static com.zenith.util.Constants.CONFIG;
 
 public class JoinGamePostHandler implements HandlerRegistry.PostOutgoingHandler<ServerJoinGamePacket, ServerConnection> {
     @Override
@@ -27,6 +29,18 @@ public class JoinGamePostHandler implements HandlerRegistry.PostOutgoingHandler<
                     session.send(connection.getEntitySpawnPacket());
                     session.send(connection.getEntityMetadataPacket());
                 });
+        if (CONFIG.client.extra.chat.hideChat) {
+            session.send(new ServerChatPacket("§7Chat is currently disabled. To enable chat, type §c/togglechat§7.", true));
+        }
+        if (CONFIG.client.extra.chat.hideWhispers) {
+            session.send(new ServerChatPacket("§7Whispers are currently disabled. To enable whispers, type §c/toggleprivatemsgs§7.", true));
+        }
+        if (CONFIG.client.extra.clientConnectionMessages) {
+            session.send(new ServerChatPacket("§7Connection messages enabled. To disable, type §c/toggleconnectionmsgs§7.", true));
+        }
+        if (CONFIG.client.extra.chat.hideDeathMessages) {
+            session.send(new ServerChatPacket("§7Death messages are currently disabled. To enable death messages, type §c/toggledeathmsgs§7.", true));
+        }
     }
 
     @Override
