837e4f12bc93f0214d337bcab9b85102ebe62486
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-08-03 22:38:02 +0000

    handle linux py launch with env

--- launch.sh
+++ launch.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+PYTHON_CMD="python"
+
+# Check if 'python' is not found, then try 'python3'
+if ! command -v $PYTHON_CMD >/dev/null 2>&1; then
+  PYTHON_CMD="python3"
+fi
+
 while true
 do
   # todo: auto-download and update start.py
-  python start.py
+  $PYTHON_CMD start.py
   echo "Restarting. Press Ctrl+C to stop"
   sleep 3
 done
+
