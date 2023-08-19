798cc2c414e9c46556582bcce43255511c87b61e
odpay <odpay@yourmums.house>
2023-08-19 17:07:59 +1000

    ingame command response prefix

--- src/main/java/com/zenith/network/server/handler/player/incoming/ServerChatHandler.java
+++ src/main/java/com/zenith/network/server/handler/player/incoming/ServerChatHandler.java
@@ -45,7 +45,7 @@ public class ServerChatHandler implements IncomingHandler<ClientChatPacket, Serv
                 session.getProxy().getClient().disconnect(MANUAL_DISCONNECT);
                 return false;
             } else if ("!q".equalsIgnoreCase(message)) {
-                session.send(new ServerChatPacket(String.format("§7[§9Proxy§7]§r §7Queue: §c" + Queue.getQueueStatus().regular + " §r- §7Prio: §a" + Queue.getQueueStatus().prio, message), true));
+                session.send(new ServerChatPacket(String.format("§7[§9ZenithProxy§7]§r §7Queue: §c" + Queue.getQueueStatus().regular + " §r- §7Prio: §a" + Queue.getQueueStatus().prio, message), true));
                 return false;
             } else if (lowerCase.startsWith("!m")) {
                 session.getProxy().getActiveConnections().forEach(connection -> {
@@ -57,13 +57,13 @@ public class ServerChatHandler implements IncomingHandler<ClientChatPacket, Serv
                 if (args.size() == 1) {
                     session.getProxy().getSpectatorConnections().forEach(connection ->
                             connection.disconnect(CONFIG.server.extra.whitelist.kickmsg));
-                    session.send(new ServerChatPacket("§cAll Spectators kicked§r", true));
+                    session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cAll Spectators kicked§r", true));
                 } else {
                     final String playerName = args.get(1);
                     session.getProxy().getSpectatorConnections().stream()
                             .filter(connection -> connection.getProfileCache().getProfile().getName().equalsIgnoreCase(playerName))
                             .forEach(connection -> {
-                                session.send(new ServerChatPacket("§cKicked " + playerName + "§r", true));
+                                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cKicked " + playerName + "§r", true));
                                 connection.disconnect(CONFIG.server.extra.whitelist.kickmsg);
                             });
                 }
@@ -71,7 +71,7 @@ public class ServerChatHandler implements IncomingHandler<ClientChatPacket, Serv
             } else if (lowerCase.startsWith("!schat")) {
                 CONFIG.server.spectator.spectatorPublicChatEnabled = !CONFIG.server.spectator.spectatorPublicChatEnabled;
                 saveConfig();
-                session.send(new ServerChatPacket("§cSpectators public chat toggled " + (CONFIG.server.spectator.spectatorPublicChatEnabled ? "on" : "off") + "§r", true));
+                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cSpectators public chat toggled " + (CONFIG.server.spectator.spectatorPublicChatEnabled ? "on" : "off") + "§r", true));
                 return false;
             } else if (lowerCase.startsWith("!spectator")) {
                 CONFIG.server.spectator.allowSpectator = !CONFIG.server.spectator.allowSpectator;
@@ -79,18 +79,18 @@ public class ServerChatHandler implements IncomingHandler<ClientChatPacket, Serv
                 if (!CONFIG.server.spectator.allowSpectator) {
                     session.getProxy().getSpectatorConnections().forEach(connection -> connection.disconnect(CONFIG.server.extra.whitelist.kickmsg));
                 }
-                session.send(new ServerChatPacket("§cSpectators toggled " + (CONFIG.server.spectator.allowSpectator ? "on" : "off") + "§r", true));
+                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cSpectators toggled " + (CONFIG.server.spectator.allowSpectator ? "on" : "off") + "§r", true));
                 return false;
             } else if (lowerCase.startsWith("!sync")) {
                 PlayerCache.sync();
-                session.send(new ServerChatPacket("§cSync inventory complete", true));
+                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cSync inventory complete", true));
                 return false;
             } else if (lowerCase.startsWith("!chunksync")) {
                 ChunkCache.sync();
-                session.send(new ServerChatPacket("§cSync chunks complete", true));
+                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cSync chunks complete", true));
                 return false;
             } else {
-                session.send(new ServerChatPacket(String.format("§7[§9Proxy§7]§r §cUnknown command: §o%s", message), true));
+                session.send(new ServerChatPacket(String.format("§7[§9ZenithProxy§7]§r §cUnknown command: §o%s", message), true));
                 return false;
             }
         } else if (message.startsWith("/")) {
@@ -101,7 +101,7 @@ public class ServerChatHandler implements IncomingHandler<ClientChatPacket, Serv
                 return false;
             } else if (lowerCase.startsWith("/ignoredeathmsgs")) { // /ignoredeathmsgs <name>
                 // todo
-                session.send(new ServerChatPacket("§cNot implemented yet", true));
+                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cNot implemented yet", true));
                 return false;
             } else if (lowerCase.startsWith("/ignore")) { // can also be ignorehard which is what we treat these like anyway
                 String[] split = message.split(" ");
@@ -109,36 +109,36 @@ public class ServerChatHandler implements IncomingHandler<ClientChatPacket, Serv
                     final String player = split[1];
                     if (WHITELIST_MANAGER.isPlayerIgnored(player)) {
                         WHITELIST_MANAGER.removeIgnoreWhitelistEntryByUsername(player);
-                        session.send(new ServerChatPacket("§cRemoved " + player + " from ignore list", true));
+                        session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cRemoved " + player + " from ignore list", true));
                         return false;
                     }
                     WHITELIST_MANAGER.addIgnoreWhitelistEntryByUsername(player).ifPresentOrElse(
-                            ignoreEntry -> session.send(new ServerChatPacket("§cAdded " + ignoreEntry.username + " to ignore list", true)),
-                            () -> session.send(new ServerChatPacket("§cFailed to add " + player + " to ignore list", true))
+                            ignoreEntry -> session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cAdded " + ignoreEntry.username + " to ignore list", true)),
+                            () -> session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cFailed to add " + player + " to ignore list", true))
                     );
                 } else {
-                    session.send(new ServerChatPacket("§cInvalid syntax. Usage: /ignore <name>", true));
+                    session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cInvalid syntax. Usage: /ignore <name>", true));
                 }
                 return false;
             } else if (lowerCase.startsWith("/togglechat")) {
                 CONFIG.client.extra.chat.hideChat = !CONFIG.client.extra.chat.hideChat;
                 saveConfig();
-                session.send(new ServerChatPacket("§cChat toggled " + (CONFIG.client.extra.chat.hideChat ? "off" : "on") + "§r", true));
+                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cChat toggled " + (CONFIG.client.extra.chat.hideChat ? "off" : "on") + "§r", true));
                 return false;
             } else if (lowerCase.startsWith("/toggleprivatemsgs")) {
                 CONFIG.client.extra.chat.hideWhispers = !CONFIG.client.extra.chat.hideWhispers;
                 saveConfig();
-                session.send(new ServerChatPacket("§cWhispers messages toggled " + (CONFIG.client.extra.chat.hideWhispers ? "off" : "on") + "§r", true));
+                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cWhispers messages toggled " + (CONFIG.client.extra.chat.hideWhispers ? "off" : "on") + "§r", true));
                 return false;
             } else if (lowerCase.startsWith("/toggledeathmsgs")) {
                 CONFIG.client.extra.chat.hideDeathMessages = !CONFIG.client.extra.chat.hideDeathMessages;
                 saveConfig();
-                session.send(new ServerChatPacket("§cDeath messages toggled " + (CONFIG.client.extra.chat.hideDeathMessages ? "off" : "on") + "§r", true));
+                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cDeath messages toggled " + (CONFIG.client.extra.chat.hideDeathMessages ? "off" : "on") + "§r", true));
                 return false;
             } else if (lowerCase.startsWith("/toggleconnectionmsgs")) {
                 CONFIG.client.extra.chat.showConnectionMessages = !CONFIG.client.extra.chat.showConnectionMessages;
                 saveConfig();
-                session.send(new ServerChatPacket("§cConnection messages toggled " + (CONFIG.client.extra.chat.showConnectionMessages ? "on" : "off") + "§r", true));
+                session.send(new ServerChatPacket("§7[§9ZenithProxy§7]§r §cConnection messages toggled " + (CONFIG.client.extra.chat.showConnectionMessages ? "on" : "off") + "§r", true));
                 return false;
             }
         }
