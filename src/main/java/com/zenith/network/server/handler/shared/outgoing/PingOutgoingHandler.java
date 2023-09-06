c9a9c3f390b2ec8e1c5886b8b3eeca66bd21f9e1
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-09-06 13:21:29 -0700

    typo

--- src/main/java/com/zenith/network/server/handler/shared/outgoing/PingOutgoingHandler.java
+++ src/main/java/com/zenith/network/server/handler/shared/outgoing/PingOutgoingHandler.java
@@ -14,6 +14,6 @@ public class PingOutgoingHandler implements OutgoingHandler<ClientboundPingPacke
 
     @Override
     public Class<ClientboundPingPacket> getPacketClass() {
-        return null;
+        return ClientboundPingPacket.class;
     }
 }
