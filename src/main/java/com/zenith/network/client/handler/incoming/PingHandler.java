1edd0cc3a9abb8db5e1aa52a168029cdfdb567bb
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-09-08 14:04:31 -0700

    use sync'd ping handler

--- src/main/java/com/zenith/network/client/handler/incoming/PingHandler.java
+++ src/main/java/com/zenith/network/client/handler/incoming/PingHandler.java
@@ -4,11 +4,11 @@ import com.github.steveice10.mc.protocol.packet.ingame.clientbound.ClientboundPi
 import com.github.steveice10.mc.protocol.packet.ingame.serverbound.ServerboundPongPacket;
 import com.zenith.Proxy;
 import com.zenith.network.client.ClientSession;
-import com.zenith.network.registry.AsyncIncomingHandler;
+import com.zenith.network.registry.IncomingHandler;
 
-public class PingHandler implements AsyncIncomingHandler<ClientboundPingPacket, ClientSession> {
+public class PingHandler implements IncomingHandler<ClientboundPingPacket, ClientSession> {
     @Override
-    public boolean applyAsync(final ClientboundPingPacket packet, final ClientSession session) {
+    public boolean apply(final ClientboundPingPacket packet, final ClientSession session) {
         // grim ac uses this to determine leniency in player movements. should be synced to actual ping from player
         // todo: cancel this being sent/received to/from spectators
         if (Proxy.getInstance().getCurrentPlayer().get() == null) {
