bab05699b7cd4168e73647f60950beeaebdcdb18
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-09-26 08:59:13 -0700

    remove space at end of jvm args line

--- launcher.py
+++ launcher.py
@@ -25,7 +25,7 @@ default_java_args = """\
 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 \
 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 \
 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 \
--Djava.util.concurrent.ForkJoinPool.common.parallelism=8 -Dio.netty.allocator.maxOrder=9 -Dio.netty.eventLoopThreads=2 \ 
+-Djava.util.concurrent.ForkJoinPool.common.parallelism=8 -Dio.netty.allocator.maxOrder=9 -Dio.netty.eventLoopThreads=2 \
 -Dio.netty.leakDetection.level=disabled"""
 
 default_linux_args = """\
