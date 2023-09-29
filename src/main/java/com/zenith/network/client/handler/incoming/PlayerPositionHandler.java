236af7398c8fdf5fdf807fe2240c15cd75a02fc0
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-09-28 19:56:26 -0700

    handle tp race condition when player connecting is logging in the proxy account

--- src/main/java/com/zenith/network/client/handler/incoming/PlayerPositionHandler.java
+++ src/main/java/com/zenith/network/client/handler/incoming/PlayerPositionHandler.java
@@ -2,12 +2,16 @@ package com.zenith.network.client.handler.incoming;
 
 import com.github.steveice10.mc.protocol.data.game.entity.player.PositionElement;
 import com.github.steveice10.mc.protocol.packet.ingame.clientbound.entity.player.ClientboundPlayerPositionPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.serverbound.level.ServerboundAcceptTeleportationPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.serverbound.player.ServerboundMovePlayerPosRotPacket;
+import com.zenith.Proxy;
 import com.zenith.cache.data.PlayerCache;
 import com.zenith.feature.spectator.SpectatorUtils;
 import com.zenith.module.impl.AntiAFK;
 import com.zenith.module.impl.PlayerSimulation;
 import com.zenith.network.client.ClientSession;
 import com.zenith.network.registry.IncomingHandler;
+import com.zenith.network.server.ServerConnection;
 import lombok.NonNull;
 
 import static com.zenith.Shared.CACHE;
@@ -24,8 +28,12 @@ public class PlayerPositionHandler implements IncomingHandler<ClientboundPlayerP
                 .setZ((packet.getRelative().contains(PositionElement.Z) ? cache.getZ() : 0.0d) + packet.getZ())
                 .setYaw((packet.getRelative().contains(PositionElement.YAW) ? cache.getYaw() : 0.0f) + packet.getYaw())
                 .setPitch((packet.getRelative().contains(PositionElement.PITCH) ? cache.getPitch() : 0.0f) + packet.getPitch());
-        if (isNull(session.getProxy().getCurrentPlayer().get())) {
+        ServerConnection currentPlayer = session.getProxy().getCurrentPlayer().get();
+        if (isNull(currentPlayer)) {
             MODULE_MANAGER.get(PlayerSimulation.class).handlePlayerPosRotate(packet.getTeleportId());
+        } else if (!currentPlayer.isLoggedIn()) { // possible race condition during login where we get a position packet before we're fully logged in
+            Proxy.getInstance().getClient().send(new ServerboundAcceptTeleportationPacket(packet.getTeleportId()));
+            Proxy.getInstance().getClient().send(new ServerboundMovePlayerPosRotPacket(false, CACHE.getPlayerCache().getX(), CACHE.getPlayerCache().getY(), CACHE.getPlayerCache().getZ(), CACHE.getPlayerCache().getYaw(), CACHE.getPlayerCache().getPitch()));
         }
         SpectatorUtils.syncPlayerPositionWithSpectators();
         MODULE_MANAGER.getModule(AntiAFK.class)
