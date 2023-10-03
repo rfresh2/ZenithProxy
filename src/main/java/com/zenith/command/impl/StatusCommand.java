8d4f966564eb62281bddc849b0624de6e944cba4
odpay <odpay@odpay.net>
2023-10-03 18:43:53 +1200

    better dim nonnull check

--- src/main/java/com/zenith/command/impl/StatusCommand.java
+++ src/main/java/com/zenith/command/impl/StatusCommand.java
@@ -112,7 +112,7 @@ public class StatusCommand extends Command {
             }
             builder.addField("2b2t Queue", getQueueStatus(), true)
                     .addField("Dimension",
-                            (Proxy.getInstance().isConnected() ? CACHE.getChunkCache().getCurrentDimension().getDimensionName().replace("minecraft:", ""): "None"),
+                            (nonNull(CACHE.getChunkCache().getCurrentDimension()) ? CACHE.getChunkCache().getCurrentDimension().getDimensionName().replace("minecraft:", ""): "None"),
                             true);
             if (CONFIG.discord.reportCoords) {
                 builder.addField("Coordinates", getCoordinates(CACHE.getPlayerCache()), true);
