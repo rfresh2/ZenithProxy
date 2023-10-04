f3925ec2c44d5879313cf05e57aea877590b4451
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-04 00:10:59 -0700

    resolve srv records in ping

--- src/main/java/com/zenith/feature/queue/mcping/MCPing.java
+++ src/main/java/com/zenith/feature/queue/mcping/MCPing.java
@@ -1,12 +1,23 @@
 package com.zenith.feature.queue.mcping;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.zenith.feature.queue.mcping.data.*;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.AddressedEnvelope;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.handler.codec.dns.*;
+import io.netty.resolver.dns.DnsNameResolver;
+import io.netty.resolver.dns.DnsNameResolverBuilder;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 
 import static com.zenith.Shared.CLIENT_LOG;
 
@@ -16,10 +27,17 @@ public class MCPing {
      */
     public static final int PROTOCOL_VERSION_DISCOVERY = -1;
     private static final Gson GSON = new GsonBuilder().setLenient().create();
+    private static final String IP_REGEX = "\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b";
+    private static final EventLoopGroup EVENT_LOOP_GROUP = new NioEventLoopGroup(1, new ThreadFactoryBuilder().setNameFormat("MCPing-%d").build());
+    private static final Class<NioDatagramChannel> DATAGRAM_CHANNEL_CLASS = NioDatagramChannel.class;
 
     public ResponseDetails getPingWithDetails(PingOptions options) throws IOException {
         Pinger a = new Pinger();
-        a.setAddress(new InetSocketAddress(options.getHostname(), options.getPort()));
+        if (options.isResolveDns()) {
+            a.setAddress(resolveAddress(options.getHostname(), options.getPort()));
+        } else {
+            a.setAddress(new InetSocketAddress(options.getHostname(), options.getPort()));
+        }
         a.setTimeout(options.getTimeout());
         a.setProtocolVersion(options.getProtocolVersion());
         String json = a.fetchData();
@@ -54,6 +72,50 @@ public class MCPing {
             }
         }
         return null;
+    }
+
+    private InetSocketAddress resolveAddress(final String hostname, final int defaultPort) {
+        final String srvRecord = "_minecraft._tcp." + hostname;
+        String resolvedHostname = hostname;
+        int resolvedPort = defaultPort;
+        if(!hostname.matches(IP_REGEX) && !hostname.equalsIgnoreCase("localhost")) {
+            AddressedEnvelope<DnsResponse, InetSocketAddress> envelope = null;
+            try (DnsNameResolver resolver = new DnsNameResolverBuilder(EVENT_LOOP_GROUP.next())
+                .channelType(DATAGRAM_CHANNEL_CLASS)
+                .build()) {
+                envelope = resolver.query(new DefaultDnsQuestion(srvRecord, DnsRecordType.SRV)).get();
+
+                DnsResponse response = envelope.content();
+                if (response.count(DnsSection.ANSWER) > 0) {
+                    DefaultDnsRawRecord record = response.recordAt(DnsSection.ANSWER, 0);
+                    if (record.type() == DnsRecordType.SRV) {
+                        ByteBuf buf = record.content();
+                        buf.skipBytes(4); // Skip priority and weight.
+
+                        int port = buf.readUnsignedShort();
+                        String host = DefaultDnsRecordDecoder.decodeName(buf);
+                        if (host.endsWith(".")) {
+                            host = host.substring(0, host.length() - 1);
+                        }
+
+                        resolvedHostname = host;
+                        resolvedPort = port;
+                    }
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (envelope != null) {
+                    envelope.release();
+                }
+            }
+        }
+        try {
+            InetAddress resolved = InetAddress.getByName(resolvedHostname);
+            return new InetSocketAddress(resolved, resolvedPort);
+        } catch (UnknownHostException e) {
+            return InetSocketAddress.createUnresolved(resolvedHostname, resolvedPort);
+        }
     }
 
     public static class ResponseDetails {
