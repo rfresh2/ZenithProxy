816817eecade3a5bdfa725a032e0150905f8d542
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-03 21:56:51 -0700

    viaversion

---
+++ src/main/java/com/zenith/via/MCProxyProtocolDetectorService.java
@@ -0,0 +1,28 @@
+package com.zenith.via;
+
+import com.google.common.collect.Maps;
+import com.viaversion.viaversion.platform.AbstractProtocolDetectorService;
+
+import java.util.Map;
+
+import static com.zenith.Shared.CONFIG;
+
+// todo: might not need this
+public class MCProxyProtocolDetectorService extends AbstractProtocolDetectorService {
+    @Override
+    protected Map<String, Integer> configuredServers() {
+        final Map<String, Integer> map = Maps.newHashMap();
+        map.put(CONFIG.client.server.address, CONFIG.client.server.port);
+        return map;
+    }
+
+    @Override
+    protected int lowestSupportedProtocolVersion() {
+        return 0;
+    }
+
+    @Override
+    public void probeAllServers() {
+        // todo: uhhhh don't think this is needed
+    }
+}
