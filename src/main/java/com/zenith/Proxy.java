0c67ae563531f5102042eb9c18f21af457a1945b
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-03 21:56:51 -0700

    viaversion

--- src/main/java/com/zenith/Proxy.java
+++ src/main/java/com/zenith/Proxy.java
@@ -22,7 +22,11 @@ import com.zenith.network.server.ServerConnection;
 import com.zenith.network.server.handler.ProxyServerLoginHandler;
 import com.zenith.util.Config;
 import com.zenith.util.Wait;
+import com.zenith.via.MCProxyViaServerProxy;
 import de.themoep.minedown.adventure.MineDown;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
 import lombok.Getter;
 import lombok.Setter;
 import org.slf4j.LoggerFactory;
@@ -274,7 +278,16 @@ public class Proxy {
             this.client.setFlag(BuiltinFlags.ATTEMPT_SRV_RESOLVE, false);
         }
         this.client.setFlag(BuiltinFlags.PRINT_DEBUG, true);
-        this.client.connect(true);
+        if (CONFIG.client.viaversion.enabled) {
+            ChannelInitializer<Channel> originalChannelInitializer = this.client.buildChannelInitializer();
+            final MCProxyViaServerProxy viaProxy = new MCProxyViaServerProxy();
+            viaProxy.init();
+            ChannelInitializer<Channel> viaChannelInitializer = viaProxy.inject(originalChannelInitializer);
+            Bootstrap bootstrap = this.client.buildBootstrap(viaChannelInitializer);
+            this.client.connect(true, bootstrap);
+        } else {
+            this.client.connect(true);
+        }
     }
 
     public boolean isConnected() {
---
+++ src/main/java/com/zenith/via/handler/MCProxyViaChannelInitializer.java
@@ -0,0 +1,45 @@
+package com.zenith.via.handler;
+
+import com.viaversion.viaversion.connection.UserConnectionImpl;
+import com.viaversion.viaversion.protocol.ProtocolPipelineImpl;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import lombok.NonNull;
+
+import java.lang.reflect.Method;
+
+public class MCProxyViaChannelInitializer extends ChannelInitializer<Channel> {
+    private static final Method INIT_CHANNEL;
+
+    static {
+        try {
+            INIT_CHANNEL = ChannelInitializer.class.getDeclaredMethod("initChannel", Channel.class);
+            INIT_CHANNEL.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private final ChannelInitializer<Channel> original;
+
+    public MCProxyViaChannelInitializer(ChannelInitializer<Channel> original) {
+        this.original = original;
+    }
+
+    @Override
+    protected void initChannel(@NonNull Channel channel) throws Exception {
+        INIT_CHANNEL.invoke(original, channel);
+        UserConnectionImpl userConnection = new UserConnectionImpl(channel, true);
+        new ProtocolPipelineImpl(userConnection);
+        // outbound order before: manager -> codec -> compression -> sizer -> encryption -> readTimeout
+        // outbound order after: manager -> via-encoder -> codec -> compression -> sizer -> encryption -> readTimeout
+
+        // inbound order before: readTimeout -> encryption -> sizer -> compression -> codec -> manager
+        // inbound order after: readTimeout -> encryption -> sizer -> compression -> via-decoder -> codec -> manager
+
+        // pipeline order before readTimeout -> encryption -> sizer -> compression -> codec -> manager
+        // pipeline order after readTimeout -> encryption -> sizer -> compression -> via-encoder -> via-decoder -> codec -> manager
+        channel.pipeline().addBefore("codec", "via-encoder", new MCProxyViaEncodeHandler(userConnection));
+        channel.pipeline().addBefore("codec", "via-decoder", new MCProxyViaDecodeHandler(userConnection));
+    }
+}
