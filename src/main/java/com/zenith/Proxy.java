4f0c141ab767d5c54f149234559590f51336bbaa
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-11 15:46:31 -0700

    update description

--- src/main/java/com/zenith/Proxy.java
+++ src/main/java/com/zenith/Proxy.java
@@ -549,7 +549,9 @@ public class Proxy {
                 if (DISCORD_BOT.isRunning()) {
                     if (CONFIG.discord.manageProfileImage) DISCORD_BOT.updateProfileImage(this.serverIcon);
                     if (CONFIG.discord.manageNickname) DISCORD_BOT.setBotNickname(CONFIG.authentication.username + " | ZenithProxy");
-                    if (CONFIG.discord.manageDescription) DISCORD_BOT.setBotDescription("ZenithProxy " + LAUNCH_CONFIG.version);
+                    if (CONFIG.discord.manageDescription) DISCORD_BOT.setBotDescription("ZenithProxy " + LAUNCH_CONFIG.version
+                                                                                            + "\n\n**Discord**: https://discord.gg/nJZrSaRKtb"
+                                                                                            + "\n\nhttps://github.com/rfresh2/ZenithProxy");
                 }
             }
         } catch (Exception e) {
