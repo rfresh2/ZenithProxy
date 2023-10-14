f01d5e2348b2233fac9d37fa8ed548d118185e46
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-14 01:24:21 -0700

    database util

---
+++ src/test/java/com/zenith/database/DatabaseUtil.java
@@ -0,0 +1,25 @@
+package com.zenith.database;
+
+import com.zenith.Shared;
+import org.redisson.api.RLock;
+
+public class DatabaseUtil {
+
+//    @Test
+    public void forceUnlock() {
+        Shared.loadConfig();
+        Shared.loadLaunchConfig();
+
+        RedisClient redisClient = new RedisClient();
+
+        RLock lock = redisClient.getLock(
+            // replace with whatever database key to unlock
+            "QueueLength"
+        );
+        if (lock.forceUnlock()) {
+            System.out.println("Unlocked");
+        } else {
+            System.out.println("Failed to unlock");
+        }
+    }
+}
