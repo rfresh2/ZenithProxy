2742c1a2c6b3440a632abb21d0e1a65ecd33bda6
Marcos Jiménez <73613908+UnzorZ@users.noreply.github.com>
2023-10-17 23:55:52 +0200

    Added time warning (6h) (#43)
    * Added time warning (6h)
    
    * Changed regular message for toolbar message, added common checks and cleaned up the code
    
    * Fixed 2 missing spaces
    
    * cleanup and extend action bar render time
    
    ---------
    
    Co-authored-by: rfresh2 <89827146+rfresh2@users.noreply.github.com>

--- src/main/java/com/zenith/Proxy.java
+++ src/main/java/com/zenith/Proxy.java
@@ -4,8 +4,12 @@ import ch.qos.logback.classic.LoggerContext;
 import com.github.steveice10.mc.auth.data.GameProfile;
 import com.github.steveice10.mc.protocol.MinecraftConstants;
 import com.github.steveice10.mc.protocol.MinecraftProtocol;
+import com.github.steveice10.mc.protocol.data.game.level.sound.BuiltinSound;
+import com.github.steveice10.mc.protocol.data.game.level.sound.SoundCategory;
 import com.github.steveice10.mc.protocol.packet.ingame.clientbound.ClientboundSystemChatPacket;
 import com.github.steveice10.mc.protocol.packet.ingame.clientbound.ClientboundTabListPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.clientbound.level.ClientboundSoundPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.clientbound.title.ClientboundSetActionBarTextPacket;
 import com.github.steveice10.packetlib.BuiltinFlags;
 import com.github.steveice10.packetlib.tcp.TcpServer;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
@@ -46,10 +50,7 @@ import java.net.URL;
 import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -156,6 +157,9 @@ public class Proxy {
             // ensure we are continuously updating the tablist even on servers that don't frequently send updates
             SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(this::tablistUpdate, 20L, 3L, TimeUnit.SECONDS);
             SCHEDULED_EXECUTOR_SERVICE.submit(this::updatePrioBanStatus);
+            // 6hr kick warning
+            SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(this::sixHourKickWarningTick, 350L, 1L, TimeUnit.MINUTES);
+
             if (CONFIG.server.enabled && CONFIG.server.ping.favicon) {
                 SCHEDULED_EXECUTOR_SERVICE.submit(this::updateFavicon);
             }
@@ -565,6 +569,37 @@ public class Proxy {
             }
         } catch (Exception e) {
             SERVER_LOG.error("Unable to download server icon for \"{}\":\n", CONFIG.authentication.username, e);
+        }
+    }
+
+    public void sixHourKickWarningTick() {
+        try {
+            if (this.isPrio.orElse(false) // Prio players don't have 6h kick.
+                || !this.hasActivePlayer() // If no player is connected, nobody to warn
+                || !isOnlineOn2b2tForAtLeastDuration(Duration.ofMinutes(350)) // 6hrs - 10 mins
+            ) return;
+            final ServerConnection playerConnection = this.currentPlayer.get();
+            final int minsUntil6Hrs = (int) ((21600 - (Instant.now().getEpochSecond() - connectTime.getEpochSecond())) / 60);
+            if (minsUntil6Hrs < 0) return; // sanity check just in case 2b's plugin changes
+            var actionBarPacket = new ClientboundSetActionBarTextPacket(
+                MineDown.parse((minsUntil6Hrs <= 3 ? "&c" : "&9") + "6hr kick in: " + minsUntil6Hrs + "m"));
+            playerConnection.send(actionBarPacket);
+            // each packet will reset text render timer for 3 seconds
+            for (int i = 1; i <= 7; i++) { // render the text for about 10 seconds total
+                playerConnection.sendScheduledAsync(actionBarPacket, SCHEDULED_EXECUTOR_SERVICE, i, TimeUnit.SECONDS);
+            }
+            playerConnection.send(new ClientboundSoundPacket(
+                BuiltinSound.BLOCK_ANVIL_PLACE,
+                SoundCategory.AMBIENT,
+                CACHE.getPlayerCache().getX(),
+                CACHE.getPlayerCache().getY(),
+                CACHE.getPlayerCache().getZ(),
+                1.0f,
+                1.0f + (ThreadLocalRandom.current().nextFloat() / 10f), // slight pitch variations
+                0L
+            ));
+        } catch (final Throwable e) {
+            DEFAULT_LOG.error("Error in 6 hr kick warning tick", e);
         }
     }
 
