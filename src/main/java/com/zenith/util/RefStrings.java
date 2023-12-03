28322eae1631275b99995c5ebb535c4a75bf2614
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-12-02 21:19:39 -0800

    update protocol lib and fix graalvm build

--- src/main/java/com/zenith/util/RefStrings.java
+++ src/main/java/com/zenith/util/RefStrings.java
@@ -1,50 +1,24 @@
 package com.zenith.util;
 
 import com.google.common.base.Suppliers;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import lombok.experimental.UtilityClass;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.function.Supplier;
 
-@UtilityClass
-public class RefStrings {
-    protected static final String BRAND = "ZenithProxy";
+import static com.zenith.Shared.DEFAULT_LOG;
+
+public final class RefStrings {
+    private static final String BRAND = "ZenithProxy";
 
     public static Supplier<byte[]> BRAND_SUPPLIER = Suppliers.memoize(() -> {
-        ByteBuf buf = Unpooled.buffer(5 + BRAND.length());
-        try {
-            writeUTF8(buf, BRAND);
+        final ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream();
+        try (DataOutputStream out = new DataOutputStream(byteOutStream)) {
+            out.writeUTF(BRAND);
         } catch (IOException e) {
-            e.printStackTrace();
+            DEFAULT_LOG.error("Failed to write brand to byte array output stream!", e);
         }
-        return buf.array();
+        return byteOutStream.toByteArray();
     });
-
-    protected void writeUTF8(ByteBuf buf, String value) throws IOException {
-        final byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
-        if (bytes.length >= Short.MAX_VALUE) {
-            throw new IOException("Attempt to write a string with a length greater than Short.MAX_VALUE to ByteBuf!");
-        }
-        // Write the string's length
-        writeVarInt(buf, bytes.length);
-        buf.writeBytes(bytes);
-    }
-
-    protected void writeVarInt(ByteBuf buf, int value) {
-        byte part;
-        while (true) {
-            part = (byte) (value & 0x7F);
-            value >>>= 7;
-            if (value != 0) {
-                part |= 0x80;
-            }
-            buf.writeByte(part);
-            if (value == 0) {
-                break;
-            }
-        }
-    }
 }
