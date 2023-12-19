63bc4800cfbaed53d26d755c9c5d95d2390a8d70
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-12-19 09:46:09 -0800

    set default release channel to 1.20.1

--- src/main/java/com/zenith/util/LaunchConfig.java
+++ src/main/java/com/zenith/util/LaunchConfig.java
@@ -3,7 +3,7 @@ package com.zenith.util;
 public class LaunchConfig {
     public boolean auto_update = true;
     public boolean auto_update_launcher = true;
-    public String release_channel = "git";
+    public String release_channel = "java.1.20.1";
     public String version = "0.0.0";
     public String local_version = "0.0.0";
     public String repo_owner = "rfresh2";
