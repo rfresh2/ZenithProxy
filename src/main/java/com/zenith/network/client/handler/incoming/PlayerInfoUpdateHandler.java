5cefc9806b529fa08a02e767ecd7030d3dcd60f8
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-01-05 14:43:03 -0800

    simplify info update handler logic

--- src/main/java/com/zenith/network/client/handler/incoming/PlayerInfoUpdateHandler.java
+++ src/main/java/com/zenith/network/client/handler/incoming/PlayerInfoUpdateHandler.java
@@ -13,38 +13,30 @@ import static com.zenith.Shared.EVENT_BUS;
 public class PlayerInfoUpdateHandler implements AsyncPacketHandler<ClientboundPlayerInfoUpdatePacket, ClientSession> {
     @Override
     public boolean applyAsync(@NonNull ClientboundPlayerInfoUpdatePacket packet, @NonNull ClientSession session) {
-        if (packet.getActions().contains(ADD_PLAYER)) {
-            for (var entry : packet.getEntries()) {
+        for (int i = 0; i < packet.getEntries().length; i++) {
+            var entry = packet.getEntries()[i];
+            if (packet.getActions().contains(ADD_PLAYER)) {
                 CACHE.getTabListCache().add(entry);
                 EVENT_BUS.postAsync(new ServerPlayerConnectedEvent(entry));
             }
-        } else {
-            for (var newEntry : packet.getEntries()) {
-                CACHE.getTabListCache().get(newEntry.getProfileId()).ifPresent(entry -> {
-                    if (packet.getActions().contains(INITIALIZE_CHAT)) {
-                        entry.setSessionId(newEntry.getSessionId());
-                        entry.setExpiresAt(newEntry.getExpiresAt());
-                        entry.setKeySignature(newEntry.getKeySignature());
-                        entry.setPublicKey(newEntry.getPublicKey());
-                    }
-
-                    if (packet.getActions().contains(UPDATE_GAME_MODE)) {
-                        entry.setGameMode(newEntry.getGameMode());
-                    }
-
-                    if (packet.getActions().contains(UPDATE_LISTED)) {
-                        entry.setListed(newEntry.isListed());
-                    }
-
-                    if (packet.getActions().contains(UPDATE_LATENCY)) {
-                        entry.setLatency(newEntry.getLatency());
-                    }
-
-                    if (packet.getActions().contains(UPDATE_DISPLAY_NAME)) {
-                        entry.setDisplayName(newEntry.getDisplayName());
-                    }
-                });
-            }
+            // skip extra ops if we're only adding a player
+            if (packet.getActions().size() <= 1 && packet.getActions().contains(ADD_PLAYER)) continue;
+            CACHE.getTabListCache().get(entry.getProfileId()).ifPresent(e -> {
+                if (packet.getActions().contains(INITIALIZE_CHAT)) {
+                    e.setSessionId(entry.getSessionId());
+                    e.setExpiresAt(entry.getExpiresAt());
+                    e.setKeySignature(entry.getKeySignature());
+                    e.setPublicKey(entry.getPublicKey());
+                }
+                if (packet.getActions().contains(UPDATE_GAME_MODE))
+                    e.setGameMode(entry.getGameMode());
+                if (packet.getActions().contains(UPDATE_LISTED))
+                    e.setListed(entry.isListed());
+                if (packet.getActions().contains(UPDATE_LATENCY))
+                    e.setLatency(entry.getLatency());
+                if (packet.getActions().contains(UPDATE_DISPLAY_NAME))
+                    e.setDisplayName(entry.getDisplayName());
+            });
         }
         return true;
     }
