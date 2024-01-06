33ace7f2d0b38930dec92cd1db66a84a64db6073
Nate <me@hpfxd.com>
2024-01-05 20:14:05 -0500

    Implement world time caching (#74)
    * Cache world time
    
    Fixes world time being incorrect when joining the proxy, then suddenly jumping to the correct time when the server sends the next SetTime packet.
    
    * Reset time data on cache clear
    
    * Change WorldTimeData to a mutable object
    
    * move cache logic to cache class
    
    ---------
    
    Co-authored-by: rfresh2 <89827146+rfresh2@users.noreply.github.com>

--- src/main/java/com/zenith/cache/data/chunk/ChunkCache.java
+++ src/main/java/com/zenith/cache/data/chunk/ChunkCache.java
@@ -70,6 +70,7 @@ public class ChunkCache implements CachedData {
     // todo: also cache world border size changes
     //  doesn't particularly matter on 2b2t tho
     protected WorldBorderData worldBorderData = WorldBorderData.DEFAULT;
+    protected WorldTimeData worldTimeData;
     protected byte[] serverBrand = BRAND_SUPPLIER.get();
 
     public ChunkCache() {
@@ -349,6 +350,9 @@ public class ChunkCache implements CachedData {
                                                                    worldBorderData.getWarningTime()));
             consumer.accept(new ClientboundSetChunkCacheRadiusPacket(serverViewDistance));
             consumer.accept(new ClientboundSetChunkCacheCenterPacket(centerX, centerZ));
+            if (this.worldTimeData != null) {
+                consumer.accept(this.worldTimeData.toPacket());
+            }
             readCache(() -> {
                 for (final Chunk chunk : this.cache.values()) {
                     consumer.accept(new ClientboundLevelChunkWithLightPacket(
@@ -388,6 +392,7 @@ public class ChunkCache implements CachedData {
                 this.serverSimulationDistance = -1;
                 this.registryTag = null;
                 this.worldBorderData = WorldBorderData.DEFAULT;
+                this.worldTimeData = null;
                 this.serverBrand = BRAND_SUPPLIER.get();
             }
             return true;
@@ -529,5 +534,10 @@ public class ChunkCache implements CachedData {
                                        packet.isDebug(),
                                        packet.isFlat());
         CACHE_LOG.debug("Updated current dimension to {}", currentDimension.dimensionName);
+    }
+
+    public void updateWorldTime(final ClientboundSetTimePacket packet) {
+        if (this.worldTimeData == null) this.worldTimeData = new WorldTimeData();
+        else this.worldTimeData.update(packet);
     }
 }
