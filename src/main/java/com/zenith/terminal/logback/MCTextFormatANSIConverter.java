c7886b1e5955c58f80fe9255411c3b88ca69a4b7
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-01-09 12:35:11 -0800

    fix kyori ansi encoder colors on non-standard terminals

--- src/main/java/com/zenith/terminal/logback/MCTextFormatANSIConverter.java
+++ src/main/java/com/zenith/terminal/logback/MCTextFormatANSIConverter.java
@@ -5,10 +5,16 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.zenith.util.ComponentSerializer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.ansi.ANSIComponentSerializer;
+import net.kyori.ansi.ColorLevel;
 
 public class MCTextFormatANSIConverter extends MessageConverter {
     private static final char ESC = 0x1B;
     private static final String finalizer = ESC + "[0m";
+
+    static {
+        final String TERM = System.getenv("TERM");
+        if (TERM == null) System.setProperty(ColorLevel.COLOR_LEVEL_PROPERTY, "indexed16");
+    }
 
     @Override
     public String convert(ILoggingEvent event) {
