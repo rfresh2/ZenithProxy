5567a9cd3154131c1d415697c0d1219816d2b1df
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-01-22 15:41:10 -0800

    uberlauncher

--- launcher.py
+++ launcher.py
@@ -1,3 +1,4 @@
+import hashlib
 import http.client
 import json
 import os
@@ -19,6 +20,8 @@ repo_name = "ZenithProxy"
 launch_dir = "launcher/"
 custom_jvm_args = None
 system = platform.system()
+launcher_tag = "launcher"
+
 
 default_java_args = """\
 -Xmx300m \
@@ -48,11 +51,9 @@ def init_launch_config():
             return data
     except FileNotFoundError:
         print("launch_config.json not found")
-        create_default_launch_config()
         return None
     except json.decoder.JSONDecodeError:
         print("launch_config.json is invalid")
-        create_default_launch_config()
         return None
 
 
@@ -242,7 +243,30 @@ def get_release_asset_id(release_id, asset_name):
             )
             return asset_id
         else:
-            print("Failed to get release asset ID:", response.status, response.reason)
+            print("Failed to get release asset ID:", get_github_api_base_url() + url, response.status, response.reason)
+            return None
+    except Exception as e:
+        print("Failed to get release asset ID:", e)
+        return None
+    finally:
+        connection.close()
+
+
+def get_release_tag_asset_id(release_id, asset_name):
+    url = f"/repos/{repo_owner}/{repo_name}/releases/tags/{release_id}"
+    try:
+        connection = http.client.HTTPSConnection(get_github_api_base_url(), context=get_ssl_context())
+        connection.request("GET", url, headers=get_github_base_headers())
+        response = connection.getresponse()
+        if response.status == 200:
+            release_data = json.loads(response.read())
+            asset_id = next(
+                (asset["id"] for asset in release_data["assets"] if asset["name"] == asset_name),
+                None
+            )
+            return asset_id
+        else:
+            print("Failed to get release asset ID:", get_github_api_base_url() + url, response.status, response.reason)
             return None
     except Exception as e:
         print("Failed to get release asset ID:", e)
@@ -440,111 +464,368 @@ def validate_system_with_config():
     else:
         return False
 
+def validate_linux_system():
+    return system == "Linux" and validate_linux_cpu_flags() and validate_linux_glibc_version()
+
 
 def critical_error(message):
     print("CRITICAL: " + message)
     sys.exit(69)
 
 
-# Check our release channel and version
+def setup_exec():
+    if validate_linux_system():  # otherwise we will always select java
+        while True:
+            print("Select a ZenithProxy platform: (1/2)")
+            print("1. java")
+            print("2. linux")
+            i1 = input("> ")
+            if i1 == "1":
+                release_channel = "java"
+                break
+            elif i1 == "2":
+                release_channel = "linux"
+                break
+            else:
+                print("Invalid input. Enter 1 or 2")
+    else:
+        print("Auto-selecting the java release channel based on current system")
+        release_channel = "java"
 
+    while True:
+        print("Select a Minecraft version: (1/2)")
+        print("1. 1.20.1")
+        print("2. 1.20.4")
+        i1 = input("> ")
+        if i1 == "1":
+            minecraft_version = "1.20.1"
+            break
+        elif i1 == "2":
+            minecraft_version = "1.20.4"
+            break
+        else:
+            print("Invalid input. Enter 1 or 2")
+
+    launch_config = {
+        "auto_update": True,
+        "auto_update_launcher": True,
+        "release_channel": release_channel + "." + minecraft_version,
+        "version": "0.0.0",
+        "local_version": "0.0.0",
+        "repo_owner": "rfresh2",
+        "repo_name": "ZenithProxy"
+    }
+
+    with open("launch_config.json", "w") as f:
+        f.write(json.dumps(launch_config, indent=2))
+        print("launch_config written successfully")
+
+    if os.path.exists("config.json"):
+        while True:
+            print("config.json already exists, overwrite and continue anyway? (y/n)")
+            i1 = input("> ").lower()
+            if i1 == "n":
+                print("Setup complete!")
+                print("Run './launch.sh' (Unix) or '.\\launch.bat` (Windows) to start ZenithProxy!")
+                exit(0)
+            elif i1 == "y":
+                break
+            else:
+                print("Invalid input. Enter y or n")
+
+    while True:
+        print("Select authentication method: (1/2)")
+        print("1. Device Code (Recommended)")
+        print("2. Username and Password")
+
+        i1 = input("> ")
+        if i1 == "1":
+            auth_method = "device_code"
+            break
+        elif i1 == "2":
+            auth_method = "msa"
+            break
+        else:
+            print("Invalid input. Enter 1 or 2")
+
+    if auth_method == "msa":
+        while True:
+            print("Enter your Microsoft account email/username:")
+            username = input("> ")
+            if "@" in username:
+                break
+        while True:
+            print("Enter your Microsoft account password:")
+            password = input("> ")
+            if len(password) > 0:
+                break
+
+    while True:
+        print("Input port the proxy will listen on (e.g. 25565):")
+        port = input("> ")
+        try:
+            port = int(port)
+            if port < 1 or port > 65535:
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid port number. Must be between 1 and 65535")
+
+    while True:
+        print("Input the IP address players should connect to. This can be a domain name or an IP address.")
+        print("If you are unsure, leave this blank and the proxy will use the IP address of the machine it is running on.")
+        print("If you are using a domain name, make sure you have DNS records set up (see README.md)")
+        ip = input("> ")
+        if ip == "":
+            connection = http.client.HTTPSConnection("api.ipify.org")
+            connection.request("GET", "/")
+            response = connection.getresponse()
+            if response.status == 200:
+                ip = response.read().decode()
+                break
+            else:
+                print("Failed to get IP address:", response.status, response.reason)
+        else:
+            break
+
+    while True:
+        print("Enable Discord bot? (y/n)")
+        i2 = input("> ")
+        if i2 == "y":
+            discord_bot = True
+            break
+        elif i2 == "n":
+            discord_bot = False
+            break
+        else:
+            print("Invalid input. Enter y or n")
+
+    if discord_bot:
+        while True:
+            print("See README.md for instructions on how to set up a Discord bot")
+            print("Enter your Discord bot token:")
+            discord_bot_token = input("> ")
+            if len(discord_bot_token) > 50:
+                break
+            else:
+                print("Invalid token")
+        while True:
+            print("Enter the channel ID the proxy will be managed in:")
+            discord_channel_id = input("> ")
+            try:
+                discord_channel_id = int(discord_channel_id)
+                if discord_channel_id < 1000000000 or discord_channel_id > 9999999999999999999:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Invalid ID")
+        while True:
+            print("Enter the role ID of the role that will be able to manage the proxy's whitelist:")
+            discord_admin_role_id = input("> ")
+            try:
+                discord_admin_role_id = int(discord_admin_role_id)
+                if discord_admin_role_id < 1000000000 or discord_admin_role_id > 9999999999999999999:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Invalid ID")
+        while True:
+            print("Enter the channel ID the proxy will relay chat messages to:")
+            discord_chat_relay_channel = input("> ")
+            try:
+                discord_chat_relay_channel = int(discord_chat_relay_channel)
+                if discord_chat_relay_channel < 1000000000 or discord_chat_relay_channel > 9999999999999999999:
+                    raise ValueError
+                if discord_chat_relay_channel == discord_channel_id:
+                    print("Chat relay channel cannot be the same as the management channel")
+                    continue
+                break
+            except ValueError:
+                print("Invalid ID")
+
+    # Write config.json
+    config = {}
+
+    if auth_method == "msa":
+        config["authentication"] = {
+            "accountType": "msa",
+            "email": username,
+            "password": password
+        }
+
+    ipPattern = re.compile(r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+    proxyAddress = ip + ":" + str(port) if ipPattern.match(ip) else ip
+
+    config["server"] = {
+        "bind": {
+            "port": port,
+        },
+        "proxyIP": proxyAddress
+    }
+
+    if discord_bot:
+        config["discord"] = {
+            "enable": True,
+            "token": discord_bot_token,
+            "channelId": discord_channel_id,
+            "accountOwnerRoleId": discord_admin_role_id,
+            "chatRelay": {
+                "enable": True,
+                "channelId": discord_chat_relay_channel
+            }
+        }
+
+    with open("config.json", "w") as f:
+        f.write(json.dumps(config, indent=2))
+        print("config.json written successfully!")
+
+    print("Setup complete!")
+    # print("Run './launch.sh' (Unix) or '.\\launch.bat` (Windows) to start ZenithProxy!")
+
+
+def update_launcher_exec():
+    if not auto_update_launcher:
+        return
+    print("Checking for launcher update...")
+    launcherAssetID = get_release_tag_asset_id(launcher_tag, "launcher.py")
+    if launcherAssetID is None:
+        exit(1)
+    launcherAssetBytes = download_release_asset(launcherAssetID)
+    if launcherAssetBytes is None:
+        exit(1)
+    contents = launcherAssetBytes.decode()
+    if len(contents) < 100:
+        print("Failed to download launcher.py")
+        exit(1)
+
+    if os.path.exists("launcher.py"):
+        with open("launcher.py", "r", newline='') as f:
+            current = f.read()
+            currentHash = hashlib.sha1(current.encode()).hexdigest()
+            newHash = hashlib.sha1(contents.encode()).hexdigest()
+            if currentHash != newHash:
+                print("Updating launcher.py to " + newHash)
+                do_update = True
+    else:
+        print("No launcher.py found. Downloading from GitHub.")
+
+    if do_update:
+        with open("launcher.py.tmp", "w", newline='') as f:
+            f.write(contents)
+        os.replace("launcher.py.tmp", "launcher.py")
+        print("Updated launcher.py successfully!")
+        print("Relaunching launcher...")
+        # todo: handle native launcher executable update downloads and stuff above
+        if sys.argv[0].endswith(".py"):
+            os.execl(sys.executable, sys.executable, *sys.argv)
+        else:
+            os.execl(sys.argv[0], *sys.argv)
+
+
+def launcher_exec():
+    # Determine if there's a new update
+    # Install new update if available
+    if auto_update:
+        try:
+            if release_channel == "git":
+                git_update_check()
+            elif release_channel.startswith("java"):
+                java_update_check()
+            elif release_channel.startswith("linux"):
+                linux_native_update_check()
+        except UpdateError as e:
+            print("Error performing update check:", e)
+    elif release_channel != "git" and version != local_version:
+        print("Desired version is different from local version, attempting to download version:", version)
+        try:
+            if release_channel.startswith("java"):
+                java_get_version(version)
+            elif release_channel.startswith("linux"):
+                linux_native_get_version(version)
+        except UpdateError as e:
+            print("Error performing update check:", e)
+
+    if release_channel == "git":
+        git_read_version()
+
+    write_launch_config()
+
+    if version == "0.0.0" or local_version == "0.0.0":
+        if release_channel == "git":
+            critical_error("Invalid version found for git release channel:" + version
+                           + "\nRe-run setup.py and select another release channel.")
+        critical_error("Invalid version found:'" + version + "'"
+                       + "\nEnable `auto_updater` or specify a valid version in launch_config.json.")
+
+    if not validate_system_with_config():
+        critical_error("Invalid system for release channel: " + release_channel)
+
+    # Launch application
+
+    if release_channel == "git":
+        git_build()
+        toolchain_command = ""
+        jar_command = ""
+        if custom_jvm_args is not None and custom_jvm_args != "":
+            jvm_args = custom_jvm_args
+        else:
+            jvm_args = default_java_args
+        if system == 'Windows':
+            toolchain_command = ".\\build\\java_toolchain.bat"
+            jar_command = "-jar build\\libs\\ZenithProxy.jar"
+        else:
+            toolchain_command = "./build/java_toolchain"
+            jar_command = "-jar build/libs/ZenithProxy.jar"
+        run_script = f"{toolchain_command} {jvm_args} {jar_command}"
+        try:
+            subprocess.run(run_script, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print("Error launching application:", e)
+    elif release_channel.startswith("java"):
+        if not os.path.isfile(launch_dir + "ZenithProxy.jar"):
+            critical_error("ZenithProxy.jar not found")
+        toolchain_command = ""
+        jar_command = ""
+        if custom_jvm_args is not None and custom_jvm_args != "":
+            jvm_args = custom_jvm_args
+        else:
+            jvm_args = default_java_args
+        if system == 'Windows':
+            toolchain_command = "call java"
+            jar_command = "-jar " + launch_dir.replace("/", "\\") + "ZenithProxy.jar"
+        else:
+            toolchain_command = "java"
+            jar_command = "-jar " + launch_dir + "ZenithProxy.jar"
+        run_script = f"{toolchain_command} {jvm_args} {jar_command}"
+        try:
+            subprocess.run(run_script, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            critical_error("Error launching application:" + str(e))
+    elif release_channel.startswith("linux"):
+        if system != "Linux":
+            critical_error("Linux release channel is not supported on current system: " + system)
+        if not os.path.isfile(launch_dir + "ZenithProxy"):
+            critical_error("ZenithProxy executable not found")
+        if custom_jvm_args is not None and custom_jvm_args != "":
+            jvm_args = custom_jvm_args
+        else:
+            jvm_args = default_linux_args
+        run_script = f"./{launch_dir}ZenithProxy {jvm_args}"
+        try:
+            subprocess.run(run_script, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            critical_error("Error launching application:" + str(e))
+    else:
+        critical_error("Invalid release channel:" + release_channel)
+
+
+# Main script
 json_data = init_launch_config()
 if json_data is None:
+    print("launch_config.json not found, running setup.")
+    setup_exec()
     json_data = init_launch_config()
 read_launch_config(json_data)
 validate_launch_config()
-
-# Determine if there's a new update
-# Install new update if available
-if auto_update:
-    try:
-        if release_channel == "git":
-            git_update_check()
-        elif release_channel.startswith("java"):
-            java_update_check()
-        elif release_channel.startswith("linux"):
-            linux_native_update_check()
-    except UpdateError as e:
-        print("Error performing update check:", e)
-elif release_channel != "git" and version != local_version:
-    print("Desired version is different from local version, attempting to download version:", version)
-    try:
-        if release_channel.startswith("java"):
-            java_get_version(version)
-        elif release_channel.startswith("linux"):
-            linux_native_get_version(version)
-    except UpdateError as e:
-        print("Error performing update check:", e)
-
-if release_channel == "git":
-    git_read_version()
-
-write_launch_config()
-
-if version == "0.0.0" or local_version == "0.0.0":
-    if release_channel == "git":
-        critical_error("Invalid version found for git release channel:" + version
-                       + "\nRe-run setup.py and select another release channel.")
-    critical_error("Invalid version found:'" + version + "'"
-                   + "\nEnable `auto_updater` or specify a valid version in launch_config.json.")
-
-if not validate_system_with_config():
-    critical_error("Invalid system for release channel: " + release_channel)
-
-# Launch application
-
-if release_channel == "git":
-    git_build()
-    toolchain_command = ""
-    jar_command = ""
-    if custom_jvm_args is not None and custom_jvm_args != "":
-        jvm_args = custom_jvm_args
-    else:
-        jvm_args = default_java_args
-    if system == 'Windows':
-        toolchain_command = ".\\build\\java_toolchain.bat"
-        jar_command = "-jar build\\libs\\ZenithProxy.jar"
-    else:
-        toolchain_command = "./build/java_toolchain"
-        jar_command = "-jar build/libs/ZenithProxy.jar"
-    run_script = f"{toolchain_command} {jvm_args} {jar_command}"
-    try:
-        subprocess.run(run_script, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        print("Error launching application:", e)
-elif release_channel.startswith("java"):
-    if not os.path.isfile(launch_dir + "ZenithProxy.jar"):
-        critical_error("ZenithProxy.jar not found")
-    toolchain_command = ""
-    jar_command = ""
-    if custom_jvm_args is not None and custom_jvm_args != "":
-        jvm_args = custom_jvm_args
-    else:
-        jvm_args = default_java_args
-    if system == 'Windows':
-        toolchain_command = "call java"
-        jar_command = "-jar " + launch_dir.replace("/", "\\") + "ZenithProxy.jar"
-    else:
-        toolchain_command = "java"
-        jar_command = "-jar " + launch_dir + "ZenithProxy.jar"
-    run_script = f"{toolchain_command} {jvm_args} {jar_command}"
-    try:
-        subprocess.run(run_script, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        critical_error("Error launching application:" + str(e))
-elif release_channel.startswith("linux"):
-    if system != "Linux":
-        critical_error("Linux release channel is not supported on current system: " + system)
-    if not os.path.isfile(launch_dir + "ZenithProxy"):
-        critical_error("ZenithProxy executable not found")
-    if custom_jvm_args is not None and custom_jvm_args != "":
-        jvm_args = custom_jvm_args
-    else:
-        jvm_args = default_linux_args
-    run_script = f"./{launch_dir}ZenithProxy {jvm_args}"
-    try:
-        subprocess.run(run_script, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        critical_error("Error launching application:" + str(e))
-else:
-    critical_error("Invalid release channel:" + release_channel)
+update_launcher_exec()
+launcher_exec()
