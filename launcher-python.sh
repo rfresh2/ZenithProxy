24ad4f68dd46ea9b815270e9f4ed5b11e9e28653
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-01-23 17:08:36 -0800

    launcher-python script

---
+++ launcher-python.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+PYTHON_CMD="python"
+
+echo Finding python interpreter...
+
+# Check if 'python' is not found, then try 'python3'
+if ! command -v $PYTHON_CMD >/dev/null 2>&1; then
+  PYTHON_CMD="python3"
+fi
+
+# Check if neither 'python' nor 'python3' is found
+if ! command -v $PYTHON_CMD >/dev/null 2>&1; then
+  echo "Error: Python interpreter not found. Please install Python from https://www.python.org/downloads/."
+  exit 1
+fi
+
+echo Using Python interpreter: $PYTHON_CMD
+echo Installing dependencies...
+$PYTHON_CMD -m pip install -r src/launcher/requirements.txt
+echo Launching ZenithProxy...
+$PYTHON_CMD src/launcher/main.py
