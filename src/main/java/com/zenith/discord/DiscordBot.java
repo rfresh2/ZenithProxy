fd145888a58bd91b19f4addd7b2f0101583407bc
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-01-25 13:56:35 -0800

    fix non-prio q detection ip limit boolean

--- src/main/java/com/zenith/discord/DiscordBot.java
+++ src/main/java/com/zenith/discord/DiscordBot.java
@@ -574,13 +574,13 @@ public class DiscordBot {
             .addField("Online Duration", formatDuration(event.onlineDuration()), false)
             .color(Color.RUBY);
         if (Proxy.getInstance().isOn2b2t()
-            && Proxy.getInstance().getIsPrio().orElse(false)
+            && !Proxy.getInstance().getIsPrio().orElse(false)
             && event.reason().startsWith("You have lost connection")
             && event.onlineDuration().toSeconds() >= 0L
             && event.onlineDuration().toSeconds() <= 1L) {
             embed.description("You have likely been kicked for reaching the 2b2t non-prio account IP limit."
                                   + "\nConsider configuring a connection proxy with the `clientConnection` command."
-                                  + "\nOr migrate ZenithProxy instances to multiple hosts.");
+                                  + "\nOr migrate ZenithProxy instances to multiple hosts/IP's.");
         }
         sendEmbedMessage(embed);
         SCHEDULED_EXECUTOR_SERVICE.execute(() -> this.client.updatePresence(disconnectedPresence).block());
