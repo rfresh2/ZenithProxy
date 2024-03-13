382721bdbbba18457b023b73f52972731f8b0109
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-03-13 02:09:22 -0700

    fix equipment caching issue

--- src/main/java/com/zenith/network/client/handler/incoming/entity/SetEquipmentHandler.java
+++ src/main/java/com/zenith/network/client/handler/incoming/entity/SetEquipmentHandler.java
@@ -1,17 +1,11 @@
 package com.zenith.network.client.handler.incoming.entity;
 
-import com.github.steveice10.mc.protocol.data.game.entity.EquipmentSlot;
 import com.github.steveice10.mc.protocol.data.game.entity.metadata.Equipment;
-import com.github.steveice10.mc.protocol.data.game.entity.metadata.ItemStack;
 import com.github.steveice10.mc.protocol.packet.ingame.clientbound.entity.ClientboundSetEquipmentPacket;
-import com.zenith.cache.data.entity.Entity;
 import com.zenith.cache.data.entity.EntityLiving;
 import com.zenith.network.client.ClientSession;
 import com.zenith.network.registry.AsyncPacketHandler;
 import lombok.NonNull;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.zenith.Shared.CACHE;
 import static com.zenith.Shared.CLIENT_LOG;
@@ -19,30 +13,18 @@ import static com.zenith.Shared.CLIENT_LOG;
 public class SetEquipmentHandler implements AsyncPacketHandler<ClientboundSetEquipmentPacket, ClientSession> {
     @Override
     public boolean applyAsync(@NonNull ClientboundSetEquipmentPacket packet, @NonNull ClientSession session) {
-        try {
-            Entity entity = CACHE.getEntityCache().get(packet.getEntityId());
-            if (entity != null) {
-                if (entity instanceof EntityLiving e) {
-                    final Map<EquipmentSlot, ItemStack> equipmentMap = new HashMap<>();
-                    for (Equipment equipment : packet.getEquipment()) {
-                        if (equipment.getItem() != null) {
-                            equipmentMap.put(equipment.getSlot(), equipment.getItem());
-                        } else {
-                            equipmentMap.put(equipment.getSlot(), null);
-                        }
-                    }
-                    e.setEquipment(equipmentMap);
-                }
-            } else {
-                // can occur often due to packet ordering. non-critical to retry
-                CLIENT_LOG.debug("Received ServerEntityEquipmentPacket for invalid entity (id={})", packet.getEntityId());
-                return false;
-            }
-            return true;
-        } catch (ClassCastException e)  {
-            CLIENT_LOG.debug("Received ServerEntityEquipmentPacket for non-equipment entity (id={})", packet.getEntityId(), e);
+        var entity = CACHE.getEntityCache().get(packet.getEntityId());
+        if (entity == null) {
+            CLIENT_LOG.debug("Received ServerEntityEquipmentPacket for invalid entity (id={})", packet.getEntityId());
             return false;
         }
+        if (entity instanceof EntityLiving e) {
+            var equipmentMap = e.getEquipment();
+            for (Equipment equipment : packet.getEquipment()) {
+                equipmentMap.put(equipment.getSlot(), equipment.getItem());
+            }
+        }
+        return true;
     }
 
 }
