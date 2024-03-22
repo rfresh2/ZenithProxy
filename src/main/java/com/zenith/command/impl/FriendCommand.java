ef947643f64043fc18eec0eed3c40d3e2cceb11d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-03-21 20:28:37 -0700

    move friend list to its own command

---
+++ src/main/java/com/zenith/command/impl/FriendCommand.java
@@ -0,0 +1,75 @@
+package com.zenith.command.impl;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.zenith.command.Command;
+import com.zenith.command.CommandUsage;
+import com.zenith.command.brigadier.CommandCategory;
+import com.zenith.command.brigadier.CommandContext;
+import com.zenith.discord.Embed;
+import discord4j.rest.util.Color;
+
+import static com.mojang.brigadier.arguments.StringArgumentType.string;
+import static com.zenith.Shared.PLAYER_LISTS;
+import static com.zenith.command.util.CommandOutputHelper.playerListToString;
+import static com.zenith.discord.DiscordBot.escape;
+import static java.util.Arrays.asList;
+
+public class FriendCommand extends Command {
+    @Override
+    public CommandUsage commandUsage() {
+        return CommandUsage.args(
+            "friend",
+            CommandCategory.MANAGE,
+            """
+            Manage the friend list.
+            Friends change behavior for various modules like VisualRange, KillAura, and AutoDisconnect
+            """,
+            asList(
+                "add/del <player>",
+                "list",
+                "clear"
+            )
+        );
+    }
+
+    @Override
+    public LiteralArgumentBuilder<CommandContext> register() {
+        return command("friend")
+            .then(literal("add").then(argument("player", string()).executes(c -> {
+                final String player = StringArgumentType.getString(c, "player");
+                PLAYER_LISTS.getFriendsList().add(player)
+                    .ifPresentOrElse(e ->
+                                         c.getSource().getEmbed()
+                                             .title("Friend added"),
+                                     () -> c.getSource().getEmbed()
+                                         .title("Failed to add user: " + escape(player) + " to friends. Unable to lookup profile."));
+                return 1;
+            })))
+            .then(literal("del").then(argument("player", string()).executes(c -> {
+                final String player = StringArgumentType.getString(c, "player");
+                PLAYER_LISTS.getFriendsList().remove(player);
+                c.getSource().getEmbed()
+                    .title("Friend deleted");
+                return 1;
+            })))
+            .then(literal("list").executes(c -> {
+                c.getSource().getEmbed()
+                    .title("Friend list");
+                return 1;
+            }))
+            .then(literal("clear").executes(c -> {
+                PLAYER_LISTS.getFriendsList().clear();
+                c.getSource().getEmbed()
+                    .title("Friend list cleared!");
+                return 1;
+            }));
+    }
+
+    @Override
+    public void postPopulate(final Embed builder) {
+        builder
+            .description("**Friend List**\n" + playerListToString(PLAYER_LISTS.getFriendsList()))
+            .color(Color.CYAN);
+    }
+}
