a3cc0ba7cf84e09493847933187676e4e41a9cb4
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-03-22 21:53:54 -0700

    raycast with collision boxes and direction faces

--- src/main/java/com/zenith/feature/pathing/raycast/BlockRaycastResult.java
+++ src/main/java/com/zenith/feature/pathing/raycast/BlockRaycastResult.java
@@ -2,4 +2,8 @@ package com.zenith.feature.pathing.raycast;
 
 import com.zenith.feature.pathing.blockdata.Block;
 
-public record BlockRaycastResult(boolean hit, double x, double y, double z, Block block) { }
+public record BlockRaycastResult(boolean hit, double x, double y, double z, Direction direction, Block block) {
+    public static BlockRaycastResult miss() {
+        return new BlockRaycastResult(false, 0, 0, 0, Direction.UP, Block.AIR);
+    }
+}
