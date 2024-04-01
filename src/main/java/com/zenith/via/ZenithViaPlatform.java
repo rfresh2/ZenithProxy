ba8f6c2f8886afdf2d269d91ed31e0ce2fcb4f78
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-03-31 21:49:52 -0700

    handle via UUID's not aligning with actual player UUID's

--- src/main/java/com/zenith/via/ZenithViaPlatform.java
+++ src/main/java/com/zenith/via/ZenithViaPlatform.java
@@ -1,16 +1,19 @@
 package com.zenith.via;
 
 import com.github.steveice10.mc.protocol.packet.ingame.clientbound.ClientboundSystemChatPacket;
+import com.viaversion.viaversion.api.Via;
+import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.configuration.AbstractViaConfig;
 import com.zenith.Proxy;
+import com.zenith.network.server.ServerConnection;
 import net.kyori.adventure.text.Component;
 import net.raphimc.vialoader.impl.platform.ViaVersionPlatformImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ZenithViaPlatform extends ViaVersionPlatformImpl {
     private static final Logger LOGGER = LoggerFactory.getLogger("ViaVersion");
@@ -27,25 +30,37 @@ public class ZenithViaPlatform extends ViaVersionPlatformImpl {
 
     @Override
     public boolean kickPlayer(UUID uuid, String s) {
-        LOGGER.warn("Kicking player {} with reason: {}", uuid, s);
-        if (uuid == null) return false;
-        final AtomicBoolean kicked = new AtomicBoolean(false);
-        Proxy.getInstance().getActiveConnections().stream()
-            .filter(connection -> uuid.equals(connection.getLoginProfileUUID()))
-            .forEach(connection -> {
-                connection.disconnect(s);
-                kicked.set(true);
-            });
-        return kicked.get();
+        // the UUID does not match the logged in player's UUID
+        // viaversion sets it to the UUID we sent in the GameProfile packet, which is the proxy's UUID instead of the connecting player's
+        // or for spectators, we send the same UUID for each of them. so im not sure if this will work correctly at all
+        var connection = getServerConnection(uuid);
+        if (connection.isPresent() && !connection.get().isSpectator()) {
+            LOGGER.warn("Kicking player {} with reason: {}", uuid, s);
+            connection.get().disconnect(s);
+            return true;
+        } else {
+            LOGGER.warn("Kicking player with reason: {}", s);
+            return false; // via will still kick them by closing the tcp connection
+        }
     }
 
     @Override
     public void sendMessage(UUID uuid, String msg) {
-        LOGGER.info("Sending message: {} to player: {}", msg, uuid);
-        if (uuid != null) {
-            Proxy.getInstance().getActiveConnections().stream()
-                .filter(connection -> uuid.equals(connection.getLoginProfileUUID()))
-                .forEach(connection -> connection.send(new ClientboundSystemChatPacket(Component.text(msg), false)));
+        var connection = getServerConnection(uuid);
+        if (connection.isPresent()) {
+            LOGGER.info("Sending message: {} to player: {}", msg, uuid);
+            connection.get().send(new ClientboundSystemChatPacket(Component.text(msg), false));
+        } else {
+            LOGGER.warn("Failed to send message: {}", msg);
         }
+    }
+
+    private Optional<ServerConnection> getServerConnection(final UUID viaUuid) {
+        if (viaUuid == null) return Optional.empty();
+        UserConnection connectedClient = Via.getManager().getConnectionManager().getConnectedClient(viaUuid);
+        var channel = connectedClient.getChannel();
+        return Proxy.getInstance().getActiveConnections().stream()
+            .filter(connection -> connection.getSession().getChannel() == channel)
+            .findFirst();
     }
 }
