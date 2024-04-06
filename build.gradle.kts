078dcc2f058c5cdd9bcec950ea30c647db00f6eb
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-04-06 11:48:39 -0700

    update mcpl - fix binary translate arg serializing

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     shade("com.github.rfresh2.Discord4j:discord4j-core:b40c82ad47") {
         exclude(group = "io.netty")
     }
-    shade("com.github.rfresh2:MCProtocolLib:c454bc1888") {
+    shade("com.github.rfresh2:MCProtocolLib:15071dae02") {
         exclude(group = "io.netty.incubator")
         exclude(group = "io.netty")
         exclude(group = "com.microsoft.azure")
