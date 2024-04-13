3c994637f93fac43f0dfe60fffcfc50d2bc16e5c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-04-12 21:07:21 -0700

    jitpack'd build

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     shade("com.github.rfresh2.Discord4j:discord4j-core:b40c82ad47") {
         exclude(group = "io.netty")
     }
-    shade("com.github.rfresh2.MCProtocolLib:protocol:1.20.4-DEV") {
+    shade("com.github.rfresh2:MCProtocolLib:5ffc737853") {
         exclude(group = "io.netty.incubator")
         exclude(group = "io.netty")
         exclude(group = "com.microsoft.azure")
