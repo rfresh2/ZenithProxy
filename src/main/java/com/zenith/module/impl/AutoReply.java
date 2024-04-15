47a8a951d6abed4635dfcdf40f7ea0bedb186859
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-04-14 21:46:31 -0700

    clean up a couple modules

--- src/main/java/com/zenith/module/impl/AutoReply.java
+++ src/main/java/com/zenith/module/impl/AutoReply.java
@@ -48,25 +48,23 @@ public class AutoReply extends Module {
 
 
     public void handleServerChatReceivedEvent(ServerChatReceivedEvent event) {
-        if (isNull(Proxy.getInstance().getCurrentPlayer().get())) {
-            try {
-                if (event.isIncomingWhisper()) {
-                    if (!event.sender().get().getName().equalsIgnoreCase(CONFIG.authentication.username)
-                            && Instant.now().minus(replyRateLimitDuration).isAfter(lastReply)
-                            && (DISCORD.lastRelaymessage.isEmpty()
-                            || Instant.now().minus(Duration.ofSeconds(CONFIG.client.extra.autoReply.cooldownSeconds)).isAfter(
-                        DISCORD.lastRelaymessage.get()))) {
-                        if (isNull(repliedPlayersCache.getIfPresent(event.sender().get().getName()))) {
-                            repliedPlayersCache.put(event.sender().get().getName(), event.sender().get().getName());
-                            // 236 char max ( 256 - 4(command) - 16(max name length) )
-                            sendClientPacketAsync(new ServerboundChatPacket("/w " + event.sender().get().getName() + " " + CONFIG.client.extra.autoReply.message.substring(0, Math.min(CONFIG.client.extra.autoReply.message.length(), 236))));
-                            this.lastReply = Instant.now();
-                        }
+        if (Proxy.getInstance().hasActivePlayer()) return;
+        try {
+            if (event.isIncomingWhisper()) {
+                if (!event.sender().get().getName().equalsIgnoreCase(CONFIG.authentication.username)
+                        && Instant.now().minus(replyRateLimitDuration).isAfter(lastReply)
+                        && (DISCORD.lastRelaymessage.isEmpty()
+                        || Instant.now().minus(Duration.ofSeconds(CONFIG.client.extra.autoReply.cooldownSeconds)).isAfter(DISCORD.lastRelaymessage.get()))) {
+                    if (isNull(repliedPlayersCache.getIfPresent(event.sender().get().getName()))) {
+                        repliedPlayersCache.put(event.sender().get().getName(), event.sender().get().getName());
+                        // 236 char max ( 256 - 4(command) - 16(max name length) )
+                        sendClientPacketAsync(new ServerboundChatPacket("/w " + event.sender().get().getName() + " " + CONFIG.client.extra.autoReply.message.substring(0, Math.min(CONFIG.client.extra.autoReply.message.length(), 236))));
+                        this.lastReply = Instant.now();
                     }
                 }
-            } catch (final Throwable e) {
-                CLIENT_LOG.error("AutoReply Failed", e);
             }
+        } catch (final Throwable e) {
+            CLIENT_LOG.error("AutoReply Failed", e);
         }
     }
 }
