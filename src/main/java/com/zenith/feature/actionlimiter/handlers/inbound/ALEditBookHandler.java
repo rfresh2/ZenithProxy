c736f627f47f5d9bde69b2ee987ed17da8cdc6be
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-04-20 11:31:59 -0700

    avoid AL book signing cancel desync

--- src/main/java/com/zenith/feature/actionlimiter/handlers/inbound/ALEditBookHandler.java
+++ src/main/java/com/zenith/feature/actionlimiter/handlers/inbound/ALEditBookHandler.java
@@ -9,7 +9,15 @@ import static com.zenith.Shared.CONFIG;
 public class ALEditBookHandler implements PacketHandler<ServerboundEditBookPacket, ServerConnection> {
     @Override
     public ServerboundEditBookPacket apply(final ServerboundEditBookPacket packet, final ServerConnection session) {
-        if (!CONFIG.client.extra.actionLimiter.allowBookSigning && packet.getTitle() != null) return null;
+        if(!CONFIG.client.extra.actionLimiter.allowBookSigning
+            // if title is not null, the book is being signed
+            && packet.getTitle() != null) {
+            return new ServerboundEditBookPacket(
+                packet.getSlot(),
+                packet.getPages(),
+                null // edits the book but does not sign it
+            );
+        }
         return packet;
     }
 }
