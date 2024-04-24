cb4db81f87b60f1da4b245d0ae864a0f0d90ed96
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-04-24 01:54:02 -0700

    initialize FoodData more efficiently

--- src/main/java/com/zenith/feature/food/FoodManager.java
+++ src/main/java/com/zenith/feature/food/FoodManager.java
@@ -1,10 +1,13 @@
 package com.zenith.feature.food;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
-import java.util.List;
+import java.util.Iterator;
 import java.util.Objects;
 
 import static com.zenith.Shared.OBJECT_MAPPER;
@@ -12,7 +15,7 @@ import static java.util.Objects.nonNull;
 
 public class FoodManager {
     // key = item ID
-    private Int2ObjectMap<FoodData> foodDataMap = new Int2ObjectOpenHashMap<>();
+    private Int2ObjectMap<FoodData> foodDataMap = new Int2ObjectOpenHashMap<>(40);
 
     public FoodManager() {
         init();
@@ -25,10 +28,10 @@ public class FoodManager {
     public boolean isSafeFood(final int id) {
         final FoodData foodData = getFoodData(id);
         if (nonNull(foodData)) {
-            return !Objects.equals(foodData.getName(), "chorus_fruit")
-                    && !Objects.equals(foodData.getName(), "rotten_flesh")
-                    && !Objects.equals(foodData.getName(), "spider_eye")
-                    && !Objects.equals(foodData.getName(), "poisonous_potato");
+            return !Objects.equals(foodData.name(), "chorus_fruit")
+                    && !Objects.equals(foodData.name(), "rotten_flesh")
+                    && !Objects.equals(foodData.name(), "spider_eye")
+                    && !Objects.equals(foodData.name(), "poisonous_potato");
         }
         return false;
     }
@@ -38,9 +41,17 @@ public class FoodManager {
     }
 
     private void init() {
-        try {
-            OBJECT_MAPPER.readValue(getClass().getResourceAsStream("/mcdata/foods.json"), new TypeReference<List<FoodData>>() {} )
-                .forEach(foodData -> foodDataMap.put(foodData.getId().intValue(), foodData));
+        try (JsonParser foodParser = OBJECT_MAPPER.createParser(getClass().getResourceAsStream("/mcdata/foods.json"))) {
+            TreeNode node = foodParser.getCodec().readTree(foodParser);
+            for (Iterator<JsonNode> it = ((ArrayNode) node).elements(); it.hasNext(); ) {
+                final var e = it.next();
+                int itemId = e.get("id").asInt();
+                String itemName = e.get("name").asText();
+                int stackSize = e.get("stackSize").asInt();
+                double foodPoints = e.get("foodPoints").asDouble();
+                double saturation = e.get("saturation").asDouble();
+                foodDataMap.put(itemId, new FoodData(itemId, itemName, stackSize, foodPoints, saturation));
+            }
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
