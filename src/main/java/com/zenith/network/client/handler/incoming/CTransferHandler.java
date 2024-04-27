35e1a4acb6647cdc49cc2ceea809b728f48ce3c0
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-04-27 01:46:23 -0700

    barebones client transfer packet handler

--- src/main/java/com/zenith/network/client/handler/incoming/CTransferHandler.java
+++ src/main/java/com/zenith/network/client/handler/incoming/CTransferHandler.java
@@ -6,17 +6,20 @@ import com.zenith.network.registry.PacketHandler;
 import net.kyori.adventure.text.Component;
 import org.geysermc.mcprotocollib.protocol.packet.common.clientbound.ClientboundTransferPacket;
 
+import static com.zenith.Shared.EXECUTOR;
+
 public class CTransferHandler implements PacketHandler<ClientboundTransferPacket, ClientSession> {
     @Override
     public ClientboundTransferPacket apply(final ClientboundTransferPacket packet, final ClientSession session) {
         var reason = Component.text("Destination server requested transfer to: " + packet.getHost() + ":" + packet.getPort());
+        // todo: should we send a transfer packet to all active connections so they can reconnect to us seamlessly?
         Proxy.getInstance().getActiveConnections().forEach(c -> c.disconnect(reason));
         session.disconnect(reason);
-        // todo: implement
-        //  should we update the server configuration at all?
-        //  or should we connect to the new server ephemerally? Still need to store some state and transfer cookie state to next client session
-        //  we also need to surface what's going on to users
-//        Proxy.getInstance().connectTransfer(packet.getHost(), packet.getPort());
+        // TODO: this does not follow the protocol exactly
+        //  we need a persistent cookie store and we also need to connect with transfer intention
+        //  but these should only matter in cases of server networks where they actually require cookies
+        //  and knowing which connections are transfers or not (i.e. to block or allow them)
+        EXECUTOR.execute(() -> Proxy.getInstance().connect(packet.getHost(), packet.getPort()));
         return null;
     }
 }
