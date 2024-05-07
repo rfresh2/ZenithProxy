1224cf2ef0be04f882d92c0d7451313f24a29223
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-05-06 17:12:42 -0700

    update mcpl

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ dependencies {
     shade("com.github.rfresh2.Discord4j:discord4j-core:72e6525fc6") {
         exclude(group = "io.netty")
     }
-    shade("com.github.rfresh2:MCProtocolLib:66ab7b9bd1") {
+    shade("com.github.rfresh2:MCProtocolLib:d3c93d7216") {
         exclude(group = "io.netty.incubator")
         exclude(group = "io.netty")
         exclude(group = "com.microsoft.azure")
