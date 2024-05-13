b9444b5e1bba02e28f0dfc69f9ad1efe22edd869
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-05-13 02:16:27 -0700

    handle container actions on event loop

--- src/main/java/com/zenith/network/client/handler/postoutgoing/PostOutgoingContainerCloseHandler.java
+++ src/main/java/com/zenith/network/client/handler/postoutgoing/PostOutgoingContainerCloseHandler.java
@@ -1,16 +1,17 @@
 package com.zenith.network.client.handler.postoutgoing;
 
 import com.zenith.network.client.ClientSession;
-import com.zenith.network.registry.PostOutgoingPacketHandler;
+import com.zenith.network.registry.ClientEventLoopPacketHandler;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.inventory.ServerboundContainerClosePacket;
 
 import static com.zenith.Shared.CACHE;
 import static com.zenith.feature.spectator.SpectatorSync.syncPlayerEquipmentWithSpectatorsFromCache;
 
-public class PostOutgoingContainerCloseHandler implements PostOutgoingPacketHandler<ServerboundContainerClosePacket, ClientSession> {
+public class PostOutgoingContainerCloseHandler implements ClientEventLoopPacketHandler<ServerboundContainerClosePacket, ClientSession> {
     @Override
-    public void accept(final ServerboundContainerClosePacket packet, final ClientSession session) {
+    public boolean applyAsync(final ServerboundContainerClosePacket packet, final ClientSession session) {
         CACHE.getPlayerCache().closeContainer(packet.getContainerId());
         syncPlayerEquipmentWithSpectatorsFromCache();
+        return true;
     }
 }
