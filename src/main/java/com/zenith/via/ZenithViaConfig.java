e0d38f1b5e4dc5fff9bd4c01577d49684a2bbc63
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-05-17 16:01:56 -0700

    ViaVersion v5

--- src/main/java/com/zenith/via/ZenithViaConfig.java
+++ src/main/java/com/zenith/via/ZenithViaConfig.java
@@ -3,10 +3,11 @@ package com.zenith.via;
 import net.raphimc.vialoader.impl.viaversion.VLViaConfig;
 
 import java.io.File;
+import java.util.logging.LogManager;
 
 public class ZenithViaConfig extends VLViaConfig {
     public ZenithViaConfig(final File configFile) {
-        super(configFile);
+        super(configFile, LogManager.getLogManager().getLogger("ViaVersion"));
     }
 
     @Override
