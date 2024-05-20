c68b06c89f170e382ded45e9b0386d99f5c41a6c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-05-20 14:35:04 -0700

    force disable 2b2t zenith to server via as they added 1.20.6 support

--- src/main/java/com/zenith/via/ZenithViaInitializer.java
+++ src/main/java/com/zenith/via/ZenithViaInitializer.java
@@ -3,6 +3,7 @@ package com.zenith.via;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import com.viaversion.viaversion.connection.UserConnectionImpl;
 import com.viaversion.viaversion.protocol.ProtocolPipelineImpl;
+import com.zenith.Proxy;
 import com.zenith.via.handler.ZViaProtocolStateHandler;
 import io.netty.channel.Channel;
 import net.raphimc.vialoader.ViaLoader;
@@ -39,11 +40,9 @@ public class ZenithViaInitializer {
         if (CONFIG.client.viaversion.autoProtocolVersion) updateClientViaProtocolVersion();
         if (CONFIG.client.viaversion.protocolVersion == MinecraftCodec.CODEC.getProtocolVersion()) {
             CLIENT_LOG.warn("ViaVersion enabled but the protocol is the same as ours, connecting without ViaVersion");
+        } else if (Proxy.getInstance().isOn2b2t()) {
+            CLIENT_LOG.warn("ViaVersion enabled but server set to 2b2t.org, connecting without ViaVersion");
         }
-        // todo: add back when 2b2t adds 1.20.6 client support
-//        else if (Proxy.getInstance().isOn2b2t()) {
-//            CLIENT_LOG.warn("ViaVersion enabled but server set to 2b2t.org, connecting without ViaVersion");
-//        }
         else {
             init();
             UserConnectionImpl userConnection = new UserConnectionImpl(channel, true);
