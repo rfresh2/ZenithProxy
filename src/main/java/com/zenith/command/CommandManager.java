dccf22f38fc6b934c92dfc610967ace057112381
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-07-03 00:36:08 -0700

    1.21.5 deprecation notification

--- src/main/java/com/zenith/command/CommandManager.java
+++ src/main/java/com/zenith/command/CommandManager.java
@@ -57,6 +57,7 @@ public class CommandManager {
         new CoordinateObfuscationCommand(),
         new DatabaseCommand(),
         new DebugCommand(),
+        new DeprecationWarningCommand(),
         new DisconnectCommand(),
         new DiscordManageCommand(),
         new DiscordNotificationsCommand(),
