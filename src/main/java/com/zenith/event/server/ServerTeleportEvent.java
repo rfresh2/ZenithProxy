b217ca5d0990adc20605851bc779de80f981a645
naiski <8918672+naiski@users.noreply.github.com>
2025-07-07 17:37:09 -0400

    Added ServerTeleportEvent

---
+++ src/main/java/com/zenith/event/server/ServerTeleportEvent.java
@@ -0,0 +1,3 @@
+package com.zenith.event.server;
+
+public class ServerTeleportEvent { }
--- src/main/java/com/zenith/network/client/handler/postoutgoing/PostOutgoingAcceptTeleportHandler.java
+++ src/main/java/com/zenith/network/client/handler/postoutgoing/PostOutgoingAcceptTeleportHandler.java
@@ -1,5 +1,6 @@
 package com.zenith.network.client.handler.postoutgoing;
 
+import com.zenith.event.server.ServerTeleportEvent;
 import com.zenith.network.client.ClientSession;
 import com.zenith.network.codec.ClientEventLoopPacketHandler;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.level.ServerboundAcceptTeleportationPacket;
@@ -8,12 +9,14 @@ import java.util.NoSuchElementException;
 
 import static com.zenith.Globals.CACHE;
 import static com.zenith.Globals.CLIENT_LOG;
+import static com.zenith.Globals.EVENT_BUS;
 
 public class PostOutgoingAcceptTeleportHandler implements ClientEventLoopPacketHandler<ServerboundAcceptTeleportationPacket, ClientSession> {
 
     @Override
     public boolean applyAsync(final ServerboundAcceptTeleportationPacket packet, final ClientSession session) {
         try {
+            EVENT_BUS.postAsync(new ServerTeleportEvent());
             var queue = CACHE.getPlayerCache().getTeleportQueue();
             int expectedTeleportId = queue.firstInt();
             if (packet.getId() == expectedTeleportId) {
