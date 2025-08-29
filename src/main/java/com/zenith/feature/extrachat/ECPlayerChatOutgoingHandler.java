eab373f6071d32d7557122f1a82c59db2d5525ce
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-08-28 23:36:56 -0700

    simplify extrachat linkifier packet creation

--- src/main/java/com/zenith/feature/extrachat/ECPlayerChatOutgoingHandler.java
+++ src/main/java/com/zenith/feature/extrachat/ECPlayerChatOutgoingHandler.java
@@ -28,20 +28,7 @@ public class ECPlayerChatOutgoingHandler implements PacketHandler<ClientboundPla
         if (CONFIG.client.extra.chat.insertClickableLinks && packet.getUnsignedContent() == null) {
             var content = Component.text(packet.getContent());
             var linkify = MODULE.get(ExtraChat.class).insertClickableLinks(content);
-            return new ClientboundPlayerChatPacket(
-                packet.getSender(),
-                packet.getIndex(),
-                packet.getMessageSignature(),
-                packet.getContent(),
-                packet.getTimeStamp(),
-                packet.getSalt(),
-                packet.getLastSeenMessages(),
-                linkify,
-                packet.getFilterMask(),
-                packet.getChatType(),
-                packet.getName(),
-                packet.getTargetName()
-            );
+            return packet.withUnsignedContent(linkify);
         }
         return packet;
     }
