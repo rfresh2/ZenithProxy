8be6b0a34edfa9c6bdb0c8eba7fd7ba994e24c37
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-09-03 22:01:00 -0700

    fix replay command docs

--- src/main/java/com/zenith/command/impl/ReplayCommand.java
+++ src/main/java/com/zenith/command/impl/ReplayCommand.java
@@ -34,7 +34,7 @@ public class ReplayCommand extends Command {
             
             A `maxRecordingTime` of 0 means there is no limit, however, recording are always stopped on disconnects.
             
-            `autoStart` will automatically start a new recording when the proxy connects.
+            `autoRecord mode <mode` can automatically record while certain conditions are met.
             
             Additional recording modes can be configured in the `visualRange` command.
             """)
