6d094b44b39c94954f4ebbae4bd47c0c26681b3d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-09-08 14:15:33 -0700

    update netty

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     api("com.github.rfresh2:MCProtocolLib:$mcplVersion") {
         exclude(group = "io.netty")
     }
-    api(platform("io.netty:netty-bom:4.2.5.Final"))
+    api(platform("io.netty:netty-bom:4.2.6.Final"))
     api("io.netty:netty-buffer")
     api("io.netty:netty-codec-haproxy")
     api("io.netty:netty-codec-dns")
