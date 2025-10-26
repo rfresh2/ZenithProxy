5eacfd04a9ec785749c0c4a1e5b9b8e6ab066ed0
Ygbs <ygbs@bugcraft.org>
2025-10-26 22:10:33 +0800

    Force loading of resource packs
    Force the server to think the resource pack has been loaded, making it easier for clients of different versions to join

--- src/main/java/com/zenith/network/client/handler/incoming/ResourcePackPushHandler.java
+++ src/main/java/com/zenith/network/client/handler/incoming/ResourcePackPushHandler.java
@@ -1,24 +1,26 @@
 package com.zenith.network.client.handler.incoming;
 
-import com.zenith.Proxy;
 import com.zenith.cache.data.config.ResourcePack;
 import com.zenith.network.client.ClientSession;
 import com.zenith.network.codec.PacketHandler;
 import org.geysermc.mcprotocollib.protocol.data.game.ResourcePackStatus;
 import org.geysermc.mcprotocollib.protocol.packet.common.clientbound.ClientboundResourcePackPushPacket;
 import org.geysermc.mcprotocollib.protocol.packet.common.serverbound.ServerboundResourcePackPacket;
+import org.jspecify.annotations.NonNull;
 
 import static com.zenith.Globals.CACHE;
+import static com.zenith.Globals.CLIENT_LOG; 
 
 public class ResourcePackPushHandler implements PacketHandler<ClientboundResourcePackPushPacket, ClientSession> {
     public static final ResourcePackPushHandler INSTANCE = new ResourcePackPushHandler();
+
     @Override
-    public ClientboundResourcePackPushPacket apply(final ClientboundResourcePackPushPacket packet, final ClientSession session) {
+    public ClientboundResourcePackPushPacket apply(@NonNull final ClientboundResourcePackPushPacket packet, @NonNull final ClientSession session) {
         CACHE.getConfigurationCache().getResourcePacks().put(packet.getId(), new ResourcePack(packet.getId(), packet.getUrl(), packet.getHash(), packet.isRequired(), packet.getPrompt()));
-        if (Proxy.getInstance().hasActivePlayer()) return packet;
-        else {
-            session.sendAsync(new ServerboundResourcePackPacket(packet.getId(), ResourcePackStatus.SUCCESSFULLY_LOADED));
-            return null;
-        }
+        session.sendAsync(new ServerboundResourcePackPacket(packet.getId(), ResourcePackStatus.ACCEPTED));
+        CLIENT_LOG.debug("Spoofed resource pack status to ACCEPTED for: {}", packet.getId());
+        session.sendAsync(new ServerboundResourcePackPacket(packet.getId(), ResourcePackStatus.SUCCESSFULLY_LOADED));
+        CLIENT_LOG.debug("Spoofed resource pack status to SUCCESSFULLY_LOADED for: {}", packet.getId());
+        return null; 
     }
 }
