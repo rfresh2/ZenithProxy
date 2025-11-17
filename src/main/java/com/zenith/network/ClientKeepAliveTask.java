47ae67340b4cf82f9972fbfad1ff5453ce13779d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-11-17 15:00:30 -0800

    adjust keepalive queue timeout when no player connected

--- src/main/java/com/zenith/network/ClientKeepAliveTask.java
+++ src/main/java/com/zenith/network/ClientKeepAliveTask.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import org.geysermc.mcprotocollib.protocol.data.ProtocolState;
 import org.geysermc.mcprotocollib.protocol.packet.common.serverbound.ServerboundKeepAlivePacket;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.zenith.Globals.*;
 
 @Data
@@ -20,9 +22,14 @@ public class ClientKeepAliveTask implements Runnable {
         var keepAliveQueue = CACHE.getPlayerCache().getKeepAliveQueue();
         var keepAliveRequest = keepAliveQueue.peek();
         if (keepAliveRequest != null) {
-            var elapsed = System.currentTimeMillis() - keepAliveRequest.receivedTime();
-            var timeout = CONFIG.client.keepAliveHandling.keepAliveQueueTimeoutMs;
-            if (elapsed >= timeout || !Proxy.getInstance().hasActivePlayer()) {
+            var elapsed = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - keepAliveRequest.receivedTime());
+            // once a queue builds up we can't easily escape the backlog
+            // on keep alive receive, the handler tries to respond immediately which will get blocked for being out of order
+            // so at that point, this task is the only thing that can get us unstuck
+            var timeout = Proxy.getInstance().hasActivePlayer()
+                ? CONFIG.client.keepAliveHandling.keepAliveQueueTimeoutMs
+                : 10; // try to get unstuck asap, can't be zero so we can account for normal responding time
+            if (elapsed >= timeout) {
                 keepAliveQueue.forEach(r -> {
                     CLIENT_LOG.debug("Sending timed out KeepAlive: {} queue size: {}", r, keepAliveQueue.size());
                     client.send(new ServerboundKeepAlivePacket(r.id()));
