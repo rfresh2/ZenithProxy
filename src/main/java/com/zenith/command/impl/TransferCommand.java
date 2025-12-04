30f079f8fe91c1697ad866fe21f415a3a88f34ef
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-12-03 21:50:25 -0800

    `transfer` player arg and auto port resolver

--- src/main/java/com/zenith/command/impl/TransferCommand.java
+++ src/main/java/com/zenith/command/impl/TransferCommand.java
@@ -6,6 +6,12 @@ import com.zenith.command.api.Command;
 import com.zenith.command.api.CommandCategory;
 import com.zenith.command.api.CommandContext;
 import com.zenith.command.api.CommandUsage;
+import com.zenith.feature.queue.mcping.MCPing;
+import com.zenith.network.server.ServerSession;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
 import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
@@ -18,39 +24,92 @@ public class TransferCommand extends Command {
         return CommandUsage.builder()
             .name("transfer")
             .category(CommandCategory.MANAGE)
-            .description("Transfers connected players to a destination MC server")
+            .description("""
+                 Transfers connected players to a destination MC server.
+
+                 If no player is specified, all currently connected players will be transferred.
+                 If no port is specified, it will be looked up via DNS, or default to 25565.
+                 """)
             .usageLines(
-                "<address>"
+                "<address>",
+                "<address> <playerName>",
+                "<address> <port>",
+                "<address> <port> <playerName>"
             )
             .build();
     }
 
     @Override
     public LiteralArgumentBuilder<CommandContext> register() {
-        return command("transfer")
-            // todo: add permission check
-            // todo: argument for transferring a selected player
-            .then(argument("address", wordWithChars()).then(argument("port", integer(1, 65535)).executes(ctx -> {
-                String address = getString(ctx, "address");
-                int port = getInteger(ctx, "port");
-                var connections = Proxy.getInstance().getActiveConnections().getArray();
-                if (connections.length == 0) {
-                    ctx.getSource().getEmbed()
-                        .title("Error")
-                        .errorColor()
-                        .description("No players connected");
-                    return OK;
-                }
-                for (int i = 0; i < connections.length; i++) {
-                    var connection = connections[i];
-                    connection.transfer(address, port);
-                }
-                ctx.getSource().getEmbed()
-                    .title("Transferred")
-                    .primaryColor()
-                    .addField("Address", address, false)
-                    .addField("Port", port, false);
-                return OK;
-            })));
+        return command("transfer").requires(Command::validateAccountOwner)
+            .then(argument("address", wordWithChars())
+                .executes(ctx -> {
+                    var address = getString(ctx, "address");
+                    var port = resolvePort(address);
+                    var connections = Proxy.getInstance().getActiveConnections().getArray();
+                    transfer(ctx.getSource(), address, port, connections);
+                })
+                .then(argument("player", wordWithChars()).executes(ctx -> {
+                    var address = getString(ctx, "address");
+                    var port = resolvePort(address);
+                    var playerName = getString(ctx, "player");
+                    var connections = Proxy.getInstance().getActiveConnections().getArray();
+                    List<ServerSession> players = new ArrayList<>();
+                    for (int i = 0; i < connections.length; i++) {
+                        if (connections[i].getName().equalsIgnoreCase(playerName)) {
+                            players.add(connections[i]);
+                        }
+                    }
+                    transfer(ctx.getSource(), address, port, players.toArray(new ServerSession[0]));
+                }))
+                .then(argument("port", integer(1, 65535))
+                    .executes(ctx -> {
+                        var address = getString(ctx, "address");
+                        var port = getInteger(ctx, "port");
+                        var connections = Proxy.getInstance().getActiveConnections().getArray();
+                        transfer(ctx.getSource(), address, port, connections);
+                    })
+                    .then(argument("player", wordWithChars()).executes(ctx -> {
+                        var address = getString(ctx, "address");
+                        var port = getInteger(ctx, "port");
+                        var playerName = getString(ctx, "player");
+                        var connections = Proxy.getInstance().getActiveConnections().getArray();
+                        List<ServerSession> players = new ArrayList<>();
+                        for (int i = 0; i < connections.length; i++) {
+                            if (connections[i].getName().equalsIgnoreCase(playerName)) {
+                                players.add(connections[i]);
+                            }
+                        }
+                        transfer(ctx.getSource(), address, port, players.toArray(new ServerSession[0]));
+                    }))));
+    }
+
+    private void transfer(CommandContext ctx, String address, int port, ServerSession... connections) {
+        if (connections.length == 0) {
+            ctx.getEmbed()
+                .title("Error")
+                .errorColor()
+                .description("No players to transfer.");
+            return;
+        }
+        for (int i = 0; i < connections.length; i++) {
+            var connection = connections[i];
+            connection.transfer(address, port);
+        }
+        ctx.getEmbed()
+            .title("Transferred " + connections.length + " player(s)")
+            .primaryColor()
+            .addField("Address", address)
+            .addField("Port", port)
+            .addField("Players", String.join(", ", Stream.of(connections).map(ServerSession::getName).toList()));
+    }
+
+    private int resolvePort(String address) {
+        try {
+            var resolvedAddress = MCPing.INSTANCE.resolveAddress(address, 25565);
+            return resolvedAddress.getPort();
+        } catch (Exception e) {
+            return 25565;
+        }
     }
 }
