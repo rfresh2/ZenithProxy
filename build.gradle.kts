7c9232dbfd2ad02fad7c7a0600ac06ae1d51c4aa
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-01-19 16:16:35 -0800

    update mcpl

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
     mavenLocal()
 }
 
-val mcplVersion = "1.21.4.37"
+val mcplVersion = "1.21.4.39"
 dependencies {
     api("com.github.rfresh2:JDA:6.3.27") {
         exclude(group = "club.minnced")
