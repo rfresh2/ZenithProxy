53f7fa2e8609266bef89566564f1482314460d6e
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-02-12 14:35:13 -0800

    simplify item data serialization

--- src/main/java/com/zenith/mc/item/ItemData.java
+++ src/main/java/com/zenith/mc/item/ItemData.java
@@ -1,13 +1,20 @@
 package com.zenith.mc.item;
 
 import com.zenith.mc.RegistryData;
+import org.geysermc.mcprotocollib.protocol.data.game.item.component.DataComponentTypes;
 import org.geysermc.mcprotocollib.protocol.data.game.item.component.DataComponents;
 import org.jspecify.annotations.Nullable;
+import tools.jackson.databind.annotation.JsonDeserialize;
 
 public record ItemData(
     int id,
     String name,
-    int stackSize,
+    @JsonDeserialize(using = ItemDataComponentsDeserializer.class)
     DataComponents components,
     @Nullable ToolTag toolTag
-) implements RegistryData { }
+) implements RegistryData {
+
+    public int stackSize() {
+        return components().getOrDefault(DataComponentTypes.MAX_STACK_SIZE, 1);
+    }
+}
