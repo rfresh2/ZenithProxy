e3c2cdbd333f73157729b9055fb429da47e3f2d2
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-02-15 15:08:59 -0800

    update jda

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
 
 val mcplVersion = "1.21.4.44"
 dependencies {
-    api("com.github.rfresh2:JDA:6.3.27") {
+    api("com.github.rfresh2:JDA:6.3.28") {
         exclude(group = "club.minnced")
         exclude(group = "net.java.dev.jna")
         exclude(group = "com.google.crypto.tink")
