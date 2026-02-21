b3ebe2b23f90210c598a43fb3f97ec0d4ee3e75d
TreeOne <53233736+tr330ne@users.noreply.github.com>
2026-02-21 01:31:27 +0100

    Add blacklist management for AutoEat command

--- src/main/java/com/zenith/command/impl/AutoEatCommand.java
+++ src/main/java/com/zenith/command/impl/AutoEatCommand.java
@@ -1,12 +1,14 @@
 package com.zenith.command.impl;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.zenith.command.api.Command;
 import com.zenith.command.api.CommandCategory;
 import com.zenith.command.api.CommandContext;
 import com.zenith.command.api.CommandUsage;
 import com.zenith.discord.Embed;
+import com.zenith.mc.food.FoodRegistry;
 import com.zenith.module.impl.AutoEat;
 
 import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
@@ -70,7 +72,43 @@ public class AutoEatCommand extends Command {
                 CONFIG.client.extra.autoEat.allowUnsafeFood = getToggle(c, "toggle");
                 c.getSource().getEmbed()
                     .title("AutoEat Allow Unsafe Food " + toggleStrCaps(CONFIG.client.extra.autoEat.allowUnsafeFood));
-            })));
+            })))
+            .then(literal("blacklist")
+                .then(literal("add").then(argument("food", StringArgumentType.word()).executes(c -> {
+                    String food = StringArgumentType.getString(c, "food");
+                    if (FoodRegistry.REGISTRY.get(food) == null) {
+                        c.getSource().getEmbed()
+                            .title("AutoEat Blacklist: Unknown food \"" + food + "\"")
+                            .errorColor();
+                        return OK;
+                    }
+                    if (!CONFIG.client.extra.autoEat.blacklist.contains(food))
+                        CONFIG.client.extra.autoEat.blacklist.add(food);
+                    c.getSource().getEmbed()
+                        .title("AutoEat Blacklist: Added \"" + food + "\"");
+                    return OK;
+                })))
+                .then(literal("remove").then(argument("food", StringArgumentType.word()).executes(c -> {
+                    String food = StringArgumentType.getString(c, "food");
+                    CONFIG.client.extra.autoEat.blacklist.remove(food);
+                    c.getSource().getEmbed()
+                        .title("AutoEat Blacklist: Removed \"" + food + "\"");
+                    return OK;
+                })))
+                .then(literal("list").executes(c -> {
+                    c.getSource().getEmbed()
+                        .title("AutoEat Blacklist")
+                        .addField("Foods", CONFIG.client.extra.autoEat.blacklist.isEmpty()
+                            ? "Empty"
+                            : String.join(", ", CONFIG.client.extra.autoEat.blacklist));
+                    return OK;
+                }))
+                .then(literal("clear").executes(c -> {
+                    CONFIG.client.extra.autoEat.blacklist.clear();
+                    c.getSource().getEmbed().title("AutoEat Blacklist Cleared");
+                    return OK;
+                }))
+            );
     }
 
     @Override
