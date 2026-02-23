b66e6288880691b7abfa42cb3cc36fabfba00499
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-02-22 23:27:43 -0800

    `click right useItem`

--- src/main/java/com/zenith/command/impl/ClickCommand.java
+++ src/main/java/com/zenith/command/impl/ClickCommand.java
@@ -2,29 +2,43 @@ package com.zenith.command.impl;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.zenith.Proxy;
+import com.zenith.cache.data.inventory.Container;
 import com.zenith.command.api.Command;
 import com.zenith.command.api.CommandCategory;
 import com.zenith.command.api.CommandContext;
 import com.zenith.command.api.CommandUsage;
 import com.zenith.discord.Embed;
+import com.zenith.event.client.ClientBotTick;
+import com.zenith.feature.inventory.InventoryActionRequest;
 import com.zenith.feature.player.ClickTarget;
 import com.zenith.feature.player.Input;
 import com.zenith.feature.player.InputRequest;
+import com.zenith.mc.item.ItemRegistry;
 import com.zenith.util.config.Config.Client.Extra.Click.HoldClickTarget;
 import com.zenith.util.config.Config.Client.Extra.Click.HoldRightClickMode;
+import org.geysermc.mcprotocollib.protocol.data.game.entity.EquipmentSlot;
+import org.geysermc.mcprotocollib.protocol.data.game.entity.player.Hand;
+import org.geysermc.mcprotocollib.protocol.data.game.item.ItemStack;
+import org.geysermc.mcprotocollib.protocol.data.game.item.component.DataComponentTypes;
+import org.jspecify.annotations.NonNull;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.github.rfresh2.EventConsumer.of;
 import static com.mojang.brigadier.arguments.FloatArgumentType.floatArg;
 import static com.mojang.brigadier.arguments.FloatArgumentType.getFloat;
 import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
 import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
-import static com.zenith.Globals.CONFIG;
-import static com.zenith.Globals.INPUTS;
+import static com.zenith.Globals.*;
 import static com.zenith.command.brigadier.CustomStringArgumentType.getString;
 import static com.zenith.command.brigadier.TimeArgument.time;
 import static com.zenith.command.brigadier.ToggleArgumentType.getToggle;
 import static com.zenith.command.brigadier.ToggleArgumentType.toggle;
 
 public class ClickCommand extends Command {
+    // manual actions triggered by this command - uses very high priority
+    public static final int PRIORITY = 100000;
+
     @Override
     public CommandUsage commandUsage() {
         return CommandUsage.builder()
@@ -40,6 +54,8 @@ public class ClickCommand extends Command {
                 "left hold interval <ticks>",
                 "right",
                 "right target <any/none/entity/block>",
+                "right useItem",
+                "right useItem <mainHand/offHand>",
                 "right hold",
                 "right hold <mainHand/offHand/alternate>",
                 "right hold interval <ticks>",
@@ -77,7 +93,7 @@ public class ClickCommand extends Command {
                         .input(Input.builder()
                             .leftClick(true)
                             .build())
-                        .priority(100000)
+                        .priority(PRIORITY)
                         .build());
                     c.getSource().getEmbed()
                         .title("Left Clicked")
@@ -104,7 +120,7 @@ public class ClickCommand extends Command {
                             .leftClick(true)
                             .clickTarget(clickTarget)
                             .build())
-                        .priority(100000)
+                        .priority(PRIORITY)
                         .build());
                     c.getSource().getEmbed()
                         .title("Left Clicked")
@@ -138,13 +154,82 @@ public class ClickCommand extends Command {
                         .input(Input.builder()
                             .rightClick(true)
                             .build())
-                        .priority(100000)
+                        .priority(PRIORITY)
                         .build());
                     c.getSource().getEmbed()
                         .title("Right Clicked")
                         .primaryColor();
                     return OK;
                 })
+                .then(literal("useItem").executes(c -> {
+                    if (!Proxy.getInstance().isConnected()) {
+                        c.getSource().getEmbed()
+                            .title("Not Connected")
+                            .description("Must be connected to click");
+                        return ERROR;
+                    }
+                    var itemInMainhand = CACHE.getPlayerCache().getEquipment(EquipmentSlot.MAIN_HAND);
+                    var itemInOffhand = CACHE.getPlayerCache().getEquipment(EquipmentSlot.OFF_HAND);
+                    if (itemInMainhand == Container.EMPTY_STACK && itemInOffhand == Container.EMPTY_STACK) {
+                        c.getSource().getEmbed()
+                            .title("No Item")
+                            .description("Must have an item in mainHand or offHand");
+                        return ERROR;
+                    }
+                    var itemToUse = itemInMainhand != Container.EMPTY_STACK ? itemInMainhand : itemInOffhand;
+                    var hand = itemToUse == itemInMainhand ? Hand.MAIN_HAND : Hand.OFF_HAND;
+                    useItem(hand, itemToUse);
+                    c.getSource().getEmbed()
+                        .title("Using Item")
+                        .addField("Hand", hand.toString().toLowerCase())
+                        .addField("Item", ItemRegistry.REGISTRY.get(itemToUse.getId()).name())
+                        .primaryColor();
+                    return OK;
+                })
+                    .then(literal("mainHand").executes(c -> {
+                        if (!Proxy.getInstance().isConnected()) {
+                            c.getSource().getEmbed()
+                                .title("Not Connected")
+                                .description("Must be connected to click");
+                            return ERROR;
+                        }
+                        var itemInMainhand = CACHE.getPlayerCache().getEquipment(EquipmentSlot.MAIN_HAND);
+                        if (itemInMainhand == Container.EMPTY_STACK) {
+                            c.getSource().getEmbed()
+                                .title("No Item")
+                                .description("Must have an item in mainHand");
+                            return ERROR;
+                        }
+                        useItem(Hand.MAIN_HAND, itemInMainhand);
+                        c.getSource().getEmbed()
+                            .title("Using Item")
+                            .addField("Hand", Hand.MAIN_HAND.toString().toLowerCase())
+                            .addField("Item", ItemRegistry.REGISTRY.get(itemInMainhand.getId()).name())
+                            .primaryColor();
+                        return OK;
+                    }))
+                    .then(literal("offHand").executes(c -> {
+                        if (!Proxy.getInstance().isConnected()) {
+                            c.getSource().getEmbed()
+                                .title("Not Connected")
+                                .description("Must be connected to click");
+                            return ERROR;
+                        }
+                        var itemInOffHand = CACHE.getPlayerCache().getEquipment(EquipmentSlot.OFF_HAND);
+                        if (itemInOffHand == Container.EMPTY_STACK) {
+                            c.getSource().getEmbed()
+                                .title("No Item")
+                                .description("Must have an item in offHand");
+                            return ERROR;
+                        }
+                        useItem(Hand.OFF_HAND, itemInOffHand);
+                        c.getSource().getEmbed()
+                            .title("Using Item")
+                            .addField("Hand", Hand.MAIN_HAND.toString().toLowerCase())
+                            .addField("Item", ItemRegistry.REGISTRY.get(itemInOffHand.getId()).name())
+                            .primaryColor();
+                        return OK;
+                    })))
                 .then(literal("target").then(argument("targetType", enumStrings(HoldClickTarget.values())).executes(c -> {
                     if (!Proxy.getInstance().isConnected()) {
                         c.getSource().getEmbed()
@@ -165,7 +250,7 @@ public class ClickCommand extends Command {
                             .rightClick(true)
                             .clickTarget(clickTarget)
                             .build())
-                        .priority(100000)
+                        .priority(PRIORITY)
                         .build());
                     c.getSource().getEmbed()
                         .title("Right Clicked")
@@ -266,6 +351,42 @@ public class ClickCommand extends Command {
                             .primaryColor();
                         return OK;
                     }))));
+    }
+
+    private void useItem(Hand hand, @NonNull ItemStack item) {
+        var itemData = ItemRegistry.REGISTRY.get(item.getId());
+        var components = item.withAddedComponents(itemData.components()).getDataComponents();
+        var isConsumable = components.contains(DataComponentTypes.CONSUMABLE);
+        int ticks = isConsumable ? 50 : 0;
+        var startUse = InputRequest.builder()
+            .owner(this)
+            .input(Input.builder()
+                .clickTarget(ClickTarget.None.INSTANCE)
+                .rightClick(true)
+                .hand(hand)
+                .build())
+            .priority(PRIORITY)
+            .build();
+        var req = INPUTS.submit(startUse);
+        if (ticks > 0) {
+            req.addInputExecutedListener(f -> {
+                if (f.isAccepted()) {
+                    noActionForNextNTicks(ticks);
+                }
+            });
+        }
+    }
+
+    private void noActionForNextNTicks(final int ticks) {
+        var tickSubOwner = new Object();
+        var counter = new AtomicInteger(0);
+        EVENT_BUS.subscribe(tickSubOwner, of(ClientBotTick.class, event -> {
+            INPUTS.submit(InputRequest.noInput(this, PRIORITY));
+            INVENTORY.submit(InventoryActionRequest.noAction(this, PRIORITY));
+            if (counter.incrementAndGet() >= ticks) {
+                EVENT_BUS.unsubscribe(tickSubOwner);
+            }
+        }), of(ClientBotTick.Stopped.class, event -> EVENT_BUS.unsubscribe(tickSubOwner)));
     }
 
     @Override
