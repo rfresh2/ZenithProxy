8c8aa8d81dd78798c1b3c5584e4b81127bee5847
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-04-20 16:51:33 -0700

    mcpl fix flash particle write

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
     mavenLocal()
 }
 
-val mcplVersion = "26.1.2.3"
+val mcplVersion = "26.1.2.5"
 dependencies {
     api("com.github.rfresh2:JDA:6.4.31") {
         exclude(group = "club.minnced")
