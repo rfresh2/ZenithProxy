ad93dfa0839c659eda32fbe110fc4e280ded0bbb
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-08-05 01:15:33 -0700

    replace stream in packet handler

--- src/main/java/com/zenith/network/server/handler/spectator/outgoing/RemoveEntitiesSpectatorHandler.java
+++ src/main/java/com/zenith/network/server/handler/spectator/outgoing/RemoveEntitiesSpectatorHandler.java
@@ -6,18 +6,20 @@ import com.zenith.network.server.ServerSession;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.ClientboundSetCameraPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.entity.ClientboundRemoveEntitiesPacket;
 
-import java.util.Arrays;
-
 public class RemoveEntitiesSpectatorHandler implements PacketHandler<ClientboundRemoveEntitiesPacket, ServerSession> {
     @Override
     public ClientboundRemoveEntitiesPacket apply(final ClientboundRemoveEntitiesPacket packet, final ServerSession session) {
         var cameraTarget = session.getCameraTarget();
         if (cameraTarget != null) {
             var camEntityId = cameraTarget.getEntityId();
-            if (Arrays.stream(packet.getEntityIds()).anyMatch(id -> id == camEntityId)) {
-                session.setCameraTarget(null);
-                session.send(new ClientboundSetCameraPacket(session.getSpectatorSelfEntityId()));
-                SpectatorSync.syncSpectatorPositionToEntity(session, cameraTarget);
+            var entityIds = packet.getEntityIds();
+            for (int i = 0; i < entityIds.length; i++) {
+                if (entityIds[i] == camEntityId) {
+                    session.setCameraTarget(null);
+                    session.send(new ClientboundSetCameraPacket(session.getSpectatorSelfEntityId()));
+                    SpectatorSync.syncSpectatorPositionToEntity(session, cameraTarget);
+                    break;
+                }
             }
         }
         return packet;
