97b2beb57327155c7e8be02a6e9157a83661054d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-09-04 12:36:33 -0700

    discord file upload limit is now 20mb

--- src/main/java/com/zenith/discord/NotificationEventListener.java
+++ src/main/java/com/zenith/discord/NotificationEventListener.java
@@ -771,9 +771,9 @@ public class NotificationEventListener {
         var replayFile = event.replayFile();
         if (replayFile != null && CONFIG.client.extra.replayMod.sendRecordingsToDiscord) {
             try (InputStream in = new BufferedInputStream(new FileInputStream(replayFile))) {
-                // 10mb discord file attachment size limit
+                // 20mb discord file attachment size limit
                 long replaySizeMb = replayFile.length() / (1024 * 1024);
-                if (replaySizeMb > 10) {
+                if (replaySizeMb > 20) {
                     if (CONFIG.client.extra.replayMod.fileIOUploadIfTooLarge) {
                         DISCORD_LOG.info("Uploading large replay to file.io with size: {}", replayFile.length());
                         var notiEmbed = Embed.builder()
