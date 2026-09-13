c1e4b8a7e6f58cb50f262377d5e8eb23eb449313
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-09-12 18:24:07 -0700

    disable integ test in `nativeTest`

--- src/test/java/com/zenith/TestUtils.java
+++ src/test/java/com/zenith/TestUtils.java
@@ -2,6 +2,7 @@ package com.zenith;
 
 import com.google.gson.Gson;
 import com.zenith.util.config.Config;
+import com.zenith.util.config.LaunchConfig;
 import lombok.SneakyThrows;
 
 import java.nio.file.Files;
@@ -10,11 +11,21 @@ public class TestUtils {
 
     @SneakyThrows
     public static void setConfigFile(Config config) {
-        var tempConfigFile = Files.createTempFile("zenith-test", ".json");
+        var tempConfigFile = Files.createTempFile("zenith-test-config", ".json");
         tempConfigFile.toFile().deleteOnExit();
         try (var writer = Files.newBufferedWriter(tempConfigFile)) {
             new Gson().toJson(config, writer);
         }
         System.setProperty("zenith.config.file", tempConfigFile.toAbsolutePath().toString());
+    }
+
+    @SneakyThrows
+    public static void setLaunchConfigFile(LaunchConfig config) {
+        var tempConfigFile = Files.createTempFile("zenith-test-launch-config", ".json");
+        tempConfigFile.toFile().deleteOnExit();
+        try (var writer = Files.newBufferedWriter(tempConfigFile)) {
+            new Gson().toJson(config, writer);
+        }
+        System.setProperty("zenith.launch.config.file", tempConfigFile.toAbsolutePath().toString());
     }
 }
